a3e987c527d41f7fa79260ec671a6b9bea5aa748
Remy Degenne <remydegenne@gmail.com>
2025-05-16 16:19:26 +0200

    add details

--- BrownianMotion/Init.lean
+++ BrownianMotion/Init.lean
@@ -1,3 +1,4 @@
 import KolmogorovExtension4.KolmogorovExtension
 import Mathlib.MeasureTheory.Measure.CharacteristicFunction
 import Mathlib.Probability.Distributions.Gaussian.Basic
+import Mathlib.Probability.Moments.CovarianceBilin
