ca6409485a718852a9d983ad60a938274a79a893
Remy Degenne <remydegenne@gmail.com>
2025-05-30 22:24:15 +0200

    measurability lemmas

--- BrownianMotion/Continuity/IsKolmogorovProcess.lean
+++ BrownianMotion/Continuity/IsKolmogorovProcess.lean
@@ -19,10 +19,32 @@ variable {T Ω E : Type*} [PseudoEMetricSpace T] {mΩ : MeasurableSpace Ω}
   [PseudoEMetricSpace E] [MeasurableSpace E] [BorelSpace E]
   {p q M : ℝ≥0}
   {P : Measure Ω}
+  {X : T → Ω → E}
 
 structure IsKolmogorovProcess (X : T → Ω → E) (P : Measure Ω) (p q M : ℝ≥0) : Prop where
-  measurablePair : ∀ s t : T, Measurable (fun ω ↦ (X s ω, X t ω))
+  aemeasurablePair : ∀ s t : T, @AEMeasurable _ _ (borel (E × E)) _ (fun ω ↦ (X s ω, X t ω)) P
   kolmogorovCondition : ∀ s t : T,
     ∫⁻ ω, (edist (X s ω) (X t ω)) ^ (p : ℝ) ∂P ≤ M * edist s t ^ (q : ℝ)
+
+lemma IsKolmogorovProcess.aemeasurable (hX : IsKolmogorovProcess X P p q M) (s : T) :
+    AEMeasurable (X s) P := by
+  have : Measurable[borel (E × E), _] (Prod.fst : E × E → E) :=
+    measurable_fst.mono prod_le_borel_prod le_rfl
+  exact @Measurable.comp_aemeasurable Ω E (E × E) _ _ _ (borel (E × E)) _ _ this
+    (hX.aemeasurablePair s s)
+
+omit [MeasurableSpace E] [BorelSpace E] in
+lemma IsKolmogorovProcess.aestronglyMeasurable_edist
+    (hX : IsKolmogorovProcess X P p q M) {s t : T} :
+    AEStronglyMeasurable (fun ω ↦ edist (X s ω) (X t ω)) P := by
+  have h_str : StronglyMeasurable[borel (E × E)] (fun p : E × E ↦ edist p.1 p.2) := by
+    refine @Continuous.stronglyMeasurable _ _ (borel (E × E)) _ ?_ _ _ _ _ continuous_edist
+    refine @BorelSpace.opensMeasurable _ _ (borel (E × E)) ?_
+    exact @BorelSpace.mk _ _ (borel (E × E)) rfl
+  exact h_str.aestronglyMeasurable.comp_aemeasurable (hX.aemeasurablePair s t)
+
+omit [MeasurableSpace E] [BorelSpace E] in
+lemma IsKolmogorovProcess.aemeasurable_edist (hX : IsKolmogorovProcess X P p q M) {s t : T} :
+    AEMeasurable (fun ω ↦ edist (X s ω) (X t ω)) P := hX.aestronglyMeasurable_edist.aemeasurable
 
 end ProbabilityTheory
