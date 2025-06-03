01ac98f88cd4d372cb3c3e97c7bfb3272e7bfba1
Remy Degenne <remydegenne@gmail.com>
2025-06-03 11:09:58 +0200

    refactor logSizeBallSeq

--- BrownianMotion/Continuity/LogSizeBallSequence.lean
+++ BrownianMotion/Continuity/LogSizeBallSequence.lean
@@ -51,59 +51,109 @@ lemma card_le_logSizeRadius_ge (ha : 1 < a) (ht : t ∈ V) :
   refine (h ?_).le
   omega
 
+structure logSizeBallStruct (T : Type*) where
+  finset : Finset T
+  point : T
+  radius : ℕ
+
+noncomputable
+def logSizeBallStruct.smallBall (struct : logSizeBallStruct T) (c : ℝ≥0∞) :
+    Finset T :=
+  struct.finset.filter fun x ↦ edist struct.point x ≤ (struct.radius - 1) * c
+
+noncomputable
+def logSizeBallStruct.ball (struct : logSizeBallStruct T) (c : ℝ≥0∞) :
+    Finset T :=
+  struct.finset.filter fun x ↦ edist struct.point x ≤ struct.radius * c
+
 open Classical in
 noncomputable
-def logSizeBallSeq (J : Finset T) (hJ : J.Nonempty) (a c : ℝ≥0∞) : ℕ → Finset T × T × ℕ :=
-  Nat.rec ((J, hJ.choose, logSizeRadius hJ.choose J a c))
-    (fun _ (V, t, r) ↦
-      let V' := V.filter (fun x ↦ (r - 1) * c < edist t x)
-      let t' := if hV' : V'.Nonempty then hV'.choose else hJ.choose
-      (V', t', logSizeRadius t' V' a c))
+def logSizeBallSeq (J : Finset T) (hJ : J.Nonempty) (a c : ℝ≥0∞) : ℕ → logSizeBallStruct T :=
+  Nat.rec ({finset := J, point := hJ.choose, radius := logSizeRadius hJ.choose J a c})
+    (fun _ struct ↦
+      let V' := struct.finset \ struct.smallBall c
+      let t' := if hV' : V'.Nonempty then hV'.choose else struct.point
+      { finset := V',
+        point := t',
+        radius := logSizeRadius t' V' a c })
 
 variable {J : Finset T}
 
-lemma logSizeRadius_logSizeBallSeq_le (hJ : J.Nonempty) (hJ_card : #J ≤ a ^ n) (i : ℕ) :
-    (logSizeBallSeq J hJ a c i).2.2 ≤ n := by
+lemma finset_logSizeBallSeq_zero (hJ : J.Nonempty) :
+    (logSizeBallSeq J hJ a c 0).finset = J := rfl
+
+lemma point_logSizeBallSeq_zero (hJ : J.Nonempty) :
+    (logSizeBallSeq J hJ a c 0).point = hJ.choose := rfl
+
+lemma radius_logSizeBallSeq_zero (hJ : J.Nonempty) :
+    (logSizeBallSeq J hJ a c 0).radius = logSizeRadius hJ.choose J a c := rfl
+
+open Classical in
+lemma finset_logSizeBallSeq_add_one (hJ : J.Nonempty) (i : ℕ) :
+    (logSizeBallSeq J hJ a c (i + 1)).finset =
+      (logSizeBallSeq J hJ a c i).finset \ (logSizeBallSeq J hJ a c i).smallBall c := rfl
+
+open Classical in
+lemma point_logSizeBallSeq_add_one (hJ : J.Nonempty) (i : ℕ) :
+    (logSizeBallSeq J hJ a c (i + 1)).point
+      = if hV' : (logSizeBallSeq J hJ a c (i + 1)).finset.Nonempty then hV'.choose
+        else (logSizeBallSeq J hJ a c i).point := rfl
+
+lemma radius_logSizeBallSeq_add_one (hJ : J.Nonempty) (i : ℕ) :
+    (logSizeBallSeq J hJ a c (i + 1)).radius
+      = logSizeRadius (logSizeBallSeq J hJ a c (i + 1)).point
+          (logSizeBallSeq J hJ a c (i + 1)).finset a c := rfl
+
+lemma radius_logSizeBallSeq_le (hJ : J.Nonempty) (hJ_card : #J ≤ a ^ n) (i : ℕ) :
+    (logSizeBallSeq J hJ a c i).radius ≤ n := by
   sorry
 
-lemma logSizeBallSeq_subset (hJ : J.Nonempty) (i : ℕ) :
-    (logSizeBallSeq J hJ a c (i + 1)).1 ⊆ (logSizeBallSeq J hJ a c i).1 := by
+lemma finset_logSizeBallSeq_add_one_subset (hJ : J.Nonempty) (i : ℕ) :
+    (logSizeBallSeq J hJ a c (i + 1)).finset ⊆ (logSizeBallSeq J hJ a c i).finset := by
   sorry
 
-lemma mem_logSizeBallSeq (hJ : J.Nonempty) (i : ℕ) (h : (logSizeBallSeq J hJ a c i).1.Nonempty) :
-    (logSizeBallSeq J hJ a c i).2.1 ∈ (logSizeBallSeq J hJ a c i).1 := by
+lemma point_mem_finset_logSizeBallSeq (hJ : J.Nonempty) (i : ℕ)
+    (h : (logSizeBallSeq J hJ a c i).finset.Nonempty) :
+    (logSizeBallSeq J hJ a c i).point ∈ (logSizeBallSeq J hJ a c i).finset := by
   sorry
 
-lemma notMem_logSizeBallSeq_add_one (hJ : J.Nonempty) (i : ℕ)
+lemma point_notMem_finset_logSizeBallSeq_add_one (hJ : J.Nonempty) (i : ℕ)
     (h : (logSizeBallSeq J hJ a c i).1.Nonempty) :
-    (logSizeBallSeq J hJ a c i).2.1 ∉ (logSizeBallSeq J hJ a c (i + 1)).1 := by
+    (logSizeBallSeq J hJ a c i).point ∉ (logSizeBallSeq J hJ a c (i + 1)).finset := by
   sorry
 
-lemma card_logSizeBallSeq_add_one_lt (hJ : J.Nonempty) (i : ℕ)
-    (h : (logSizeBallSeq J hJ a c i).1.Nonempty) :
-    #(logSizeBallSeq J hJ a c (i + 1)).1 < #(logSizeBallSeq J hJ a c 1).1 := by
+lemma card_finset_logSizeBallSeq_add_one_lt (hJ : J.Nonempty) (i : ℕ)
+    (h : (logSizeBallSeq J hJ a c i).finset.Nonempty) :
+    #(logSizeBallSeq J hJ a c (i + 1)).finset < #(logSizeBallSeq J hJ a c i).finset := by
   sorry
 
-lemma card_logSizeBallSeq_le (hJ : J.Nonempty) (i : ℕ) :
-    #(logSizeBallSeq J hJ a c i).1 ≤ #J - i := by
+lemma card_finset_logSizeBallSeq_le (hJ : J.Nonempty) (i : ℕ) :
+    #(logSizeBallSeq J hJ a c i).finset ≤ #J - i := by
   sorry
 
-lemma card_logSizeBall_card_eq_zero (hJ : J.Nonempty) :
-    #(logSizeBallSeq J hJ a c #J).1 = 0 := by
+lemma card_finset_logSizeBallSeq_card_eq_zero (hJ : J.Nonempty) :
+    #(logSizeBallSeq J hJ a c #J).finset = 0 := by
+  sorry
+
+lemma disjoint_smallBall_logSizeBallSeq (hJ : J.Nonempty) (i j : ℕ) (hij : i ≠ j) :
+    Disjoint
+      ((logSizeBallSeq J hJ a c i).smallBall c) ((logSizeBallSeq J hJ a c j).smallBall c) := by
   sorry
 
 open Classical in
 noncomputable
 def pairSetSeq (J : Finset T) (a c : ℝ≥0∞) (n : ℕ) : Finset (T × T) :=
   if hJ : J.Nonempty then
-    let S := logSizeBallSeq J hJ a c
-    Finset.product {(S n).2.1} ((S n).1.filter fun x ↦ edist (S n).2.1 x ≤ (S n).2.2 * c)
+    Finset.product {(logSizeBallSeq J hJ a c n).point} ((logSizeBallSeq J hJ a c n).ball c)
   else ∅
 
 open Classical in
 noncomputable
 def pairSet (J : Finset T) (a c : ℝ≥0∞) : Finset (T × T) :=
   Finset.biUnion (Finset.range #J) (pairSetSeq J a c)
+
+lemma pairSet_subset : pairSet J a c ⊆ J.product J := by
+  sorry
 
 lemma card_pairSet_le (hJ_card : #J ≤ a ^ n) (ha : 1 < a) :
     #(pairSet J a c) ≤ a * #J := by
@@ -114,16 +164,21 @@ lemma edist_le_of_mem_pairSet (hJ_card : #J ≤ a ^ n) (ha : 1 < a) (hc : c ≠ 
     edist s t ≤ n * c := by
   sorry
 
+lemma iSup_edist_pairSet {E : Type*} [PseudoEMetricSpace E] (f : T → E) :
+    ⨆ (s) (t) (hs : s ∈ J) (ht : t ∈ J) (h : edist s t ≤ c), edist (f s) (f t)
+        ≤ 2 * ⨆ p ∈ pairSet J a c, edist (f p.1) (f p.2) := by
+  sorry
+
 theorem pair_reduction (J : Finset T) (hJ : #J ≤ a ^ n) (ha : 1 < a) (hc : c ≠ 0)
-    (E : Type*) [EDist E] :
+    (E : Type*) [PseudoEMetricSpace E] :
     ∃ K : Finset (T × T), K ⊆ J.product J
       ∧ #K ≤ a * #J
       ∧ (∀ s t, (s, t) ∈ K → edist s t ≤ n * c)
       ∧ (∀ f : T → E,
-        ⨆ (s) (t) (hs : s ∈ J) (ht : t ∈ J) (h : edist s t ≤ c), edist (f s) (f t)
+        ⨆ (s) (t) (_hs : s ∈ J) (_ht : t ∈ J) (_h : edist s t ≤ c), edist (f s) (f t)
         ≤ 2 * ⨆ p ∈ K, edist (f p.1) (f p.2)) := by
   refine ⟨pairSet J a c, ?_, ?_, ?_, ?_⟩
-  · sorry
+  · exact pairSet_subset
   · exact card_pairSet_le hJ ha
   · exact fun _ _ ↦ edist_le_of_mem_pairSet hJ ha hc
-  · sorry
+  · exact iSup_edist_pairSet
