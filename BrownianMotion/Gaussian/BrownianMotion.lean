4e0fd9d6fc9dcbedc672578fe6f137d3a2effb16
Remy Degenne <remydegenne@gmail.com>
2025-06-11 11:06:59 +0200

    define the Wiener measure

--- BrownianMotion/Gaussian/BrownianMotion.lean
+++ BrownianMotion/Gaussian/BrownianMotion.lean
@@ -18,10 +18,96 @@ namespace ProbabilityTheory
 
 def preBrownian : ℝ≥0 → (ℝ≥0 → ℝ) → ℝ := fun t ω ↦ ω t
 
+lemma measurable_preBrownian (t : ℝ≥0) : Measurable (preBrownian t) := by
+  unfold preBrownian
+  fun_prop
+
 lemma isGaussianProcess_preBrownian : IsGaussianProcess preBrownian gaussianLimit := by
   intro I
   simp only [preBrownian, Measure.map_id']
   rw [isProjectiveLimit_gaussianLimit]
   infer_instance
+
+def brownian : ℝ≥0 → (ℝ≥0 → ℝ) → ℝ := sorry
+
+lemma aemeasurable_brownian_apply (t : ℝ≥0) : AEMeasurable (brownian t) gaussianLimit := by
+  sorry
+
+lemma aemeasurable_brownian : AEMeasurable (fun ω t ↦ brownian t ω) gaussianLimit := by
+  sorry
+
+lemma continuous_brownian (ω : ℝ≥0 → ℝ) :
+    Continuous (brownian · ω) := by
+  sorry
+
+section Measure
+
+-- Subtype measurable space. The measurable space on `ℝ≥0 → ℝ` is the product of Borel σ-algebras
+-- #synth MeasurableSpace {f : ℝ≥0 → ℝ // Continuous f}
+
+noncomputable
+def wienerMeasureAux : Measure {f : ℝ≥0 → ℝ // Continuous f} :=
+  gaussianLimit.map (fun ω ↦ (⟨fun t ↦ brownian t ω, continuous_brownian ω⟩))
+
+-- Compact-open topology
+-- #synth TopologicalSpace C(ℝ≥0, ℝ)
+
+section ContinuousMap.MeasurableSpace
+
+variable {X Y : Type*} [TopologicalSpace X] [TopologicalSpace Y]
+
+instance : MeasurableSpace C(X, Y) := borel _
+instance : BorelSpace C(X, Y) where
+  measurable_eq := rfl
+
+lemma ContinuousMap.borel_eq_iSup_comap_eval [SecondCountableTopology X] [SecondCountableTopology Y]
+    [LocallyCompactSpace X] [RegularSpace Y] [MeasurableSpace Y] [BorelSpace Y] :
+    borel C(X, Y) = ⨆ a : X, (borel Y).comap fun b ↦ b a := by
+  -- https://math.stackexchange.com/questions/4789531/when-does-the-borel-sigma-algebra-of-compact-convergence-coincide-with-the-pr
+  apply le_antisymm
+  swap
+  · refine iSup_le fun x ↦ ?_
+    rw [← measurable_iff_comap_le]
+    simp_rw [← BorelSpace.measurable_eq]
+    refine Continuous.measurable ?_
+    fun_prop
+  sorry
+
+lemma ContinuousMap.measurable_iff_eval {α : Type*} [MeasurableSpace α]
+    [SecondCountableTopology X] [SecondCountableTopology Y]
+    [LocallyCompactSpace X] [RegularSpace Y] [MeasurableSpace Y] [BorelSpace Y]
+    (g : α → C(X, Y)) :
+    Measurable g ↔ ∀ (x : X), Measurable fun (a : α) ↦ g a x := by
+  rw [BorelSpace.measurable_eq (α := C(X, Y))]
+  simp_rw [BorelSpace.measurable_eq (α := Y), ContinuousMap.borel_eq_iSup_comap_eval,
+    measurable_iff_comap_le,
+    MeasurableSpace.comap_iSup, iSup_le_iff, MeasurableSpace.comap_comp, Function.comp_def]
+
+end ContinuousMap.MeasurableSpace
+
+def MeasurableEquiv.continuousMap : {f : ℝ≥0 → ℝ // Continuous f} ≃ᵐ C(ℝ≥0, ℝ) where
+  toFun := fun f ↦ ContinuousMap.mk f.1 f.2
+  invFun := fun f ↦ ⟨f, f.continuous⟩
+  left_inv f := rfl
+  right_inv f := rfl
+  measurable_toFun := by
+    simp only [Equiv.coe_fn_mk]
+    rw [ContinuousMap.measurable_iff_eval]
+    intro x
+    simp only [ContinuousMap.coe_mk]
+    revert x
+    rw [← measurable_pi_iff]
+    exact measurable_subtype_coe
+  measurable_invFun := by
+    simp only [Equiv.coe_fn_symm_mk]
+    refine Measurable.subtype_mk ?_
+    rw [measurable_pi_iff]
+    exact fun _ ↦ Continuous.measurable (by fun_prop)
+
+
+noncomputable
+def wienerMeasure : Measure C(ℝ≥0, ℝ) := wienerMeasureAux.map MeasurableEquiv.continuousMap
+
+end Measure
 
 end ProbabilityTheory
