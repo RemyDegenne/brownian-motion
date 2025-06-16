157b2a586c370101a99257612f820a618cbe6712
Etienne Marion <baguettes_casino0c@icloud.com>
2025-06-16 11:19:54 +0200

    split

--- BrownianMotion/Auxiliary/MeasureTheory.lean
+++ BrownianMotion/Auxiliary/MeasureTheory.lean
@@ -2,33 +2,12 @@ import Mathlib.MeasureTheory.Measure.CharacteristicFunction
 import Mathlib.Probability.Distributions.Gaussian.Real
 
 /-!
-# General lemmas to be upstreamed to Mathlib
+# Measure theory lemmas to be upstreamed to Mathlib
 -/
 
 open MeasureTheory
 
 open scoped NNReal ProbabilityTheory
-
-namespace ProbabilityTheory
-
-@[simp]
-lemma charFunDual_eq_charFun_toDual_symm {E : Type*} [NormedAddCommGroup E] [CompleteSpace E]
-    [InnerProductSpace ℝ E] {mE : MeasurableSpace E} {μ : Measure E} (L : NormedSpace.Dual ℝ E) :
-    charFun μ ((InnerProductSpace.toDual ℝ E).symm L) = charFunDual μ L := by
-  rw [charFun_eq_charFunDual_toDualMap]
-  congr with x
-  simp
-
-@[simp]
-lemma inner_toDual_symm_eq_self {𝕜 E : Type*} [RCLike 𝕜] [NormedAddCommGroup E]
-    [InnerProductSpace 𝕜 E] [CompleteSpace E] (L : NormedSpace.Dual 𝕜 E) :
-  inner 𝕜 ((InnerProductSpace.toDual 𝕜 E).symm L) = L := by ext; simp
-
-lemma eq_gaussianReal_integral_variance {μ : Measure ℝ} {m : ℝ} {v : ℝ≥0}
-    (h : μ = gaussianReal m v) : μ = gaussianReal μ[id] Var[id; μ].toNNReal := by
-  simp [h]
-
-end ProbabilityTheory
 
 nonrec lemma _root_.ContinuousLinearMap.integral_id_map {𝕜 E F : Type*} [RCLike 𝕜]
     [NormedAddCommGroup E] [NormedAddCommGroup F] [NormedSpace 𝕜 E] [NormedSpace ℝ E]
