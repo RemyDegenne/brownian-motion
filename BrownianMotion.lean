1063b31dacdd5c79971575003e4e85e7d41b7b77
Etienne Marion <baguettes_casino0c@icloud.com>
2025-06-16 11:02:55 +0200

    mk_all

--- BrownianMotion.lean
+++ BrownianMotion.lean
@@ -1,5 +1,4 @@
 import BrownianMotion.Auxiliary.ContinuousBilinForm
-import BrownianMotion.Auxiliary.MeasureTheory
 import BrownianMotion.Continuity.Chaining
 import BrownianMotion.Continuity.CoveringNumber
 import BrownianMotion.Continuity.HasBoundedInternalCoveringNumber
@@ -10,7 +9,6 @@ import BrownianMotion.Gaussian.BrownianMotion
 import BrownianMotion.Gaussian.CovMatrix
 import BrownianMotion.Gaussian.CovarianceBilin
 import BrownianMotion.Gaussian.Fernique
-import BrownianMotion.Gaussian.Gaussian
 import BrownianMotion.Gaussian.GaussianProcess
 import BrownianMotion.Gaussian.GramMatrix
 import BrownianMotion.Gaussian.MultivariateGaussian
