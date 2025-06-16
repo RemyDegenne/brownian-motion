2cf4507720c03d55146ccfac22c982099d739b50
Etienne Marion <baguettes_casino0c@icloud.com>
2025-06-16 22:04:40 +0200

    WIP

--- BrownianMotion/Gaussian/MultivariateGaussian.lean
+++ BrownianMotion/Gaussian/MultivariateGaussian.lean
@@ -3,9 +3,7 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import Mathlib.Analysis.CStarAlgebra.Matrix
-import Mathlib.LinearAlgebra.Matrix.PosDef
-import Mathlib.Probability.Distributions.Gaussian.Basic
+import Mathlib
 
 
 /-!
@@ -35,15 +33,34 @@ instance isProbabilityMeasure_stdGaussian : IsProbabilityMeasure (stdGaussian E)
     rw [stdGaussian, Measure.map_apply (by fun_prop) .univ]
     simp
 
-lemma integrable_eval_pi {i : Fin d} {μ : Fin d → Measure ℝ} [∀ i, IsProbabilityMeasure (μ i)]
-    {f : ℝ → ℝ} (hf : Integrable f (μ i)) :
-    Integrable (fun a ↦ f (a i)) (Measure.pi μ) := by
-  sorry
+lemma integrable_eval_pi {ι 𝕜 : Type*} [Fintype ι] [NormedCommRing 𝕜] {X : ι → Type*} {i : ι}
+    {mX : ∀ i, MeasurableSpace (X i)} {μ : (i : ι) → Measure (X i)}
+    [∀ i, IsFiniteMeasure (μ i)] {f : X i → 𝕜} (hf : Integrable f (μ i)) :
+    Integrable (fun x ↦ f (x i)) (Measure.pi μ) := by
+  classical
+  let g : Π i, X i → 𝕜 := fun j ↦ if h : j = i then h ▸ f else 1
+  have : (fun x ↦ ∏ j, g j (x j)) = fun (x : Π i, X i) ↦ f (x i) := by
+    ext x
+    rw [show f (x i) = g i (x i) by simp [g]]
+    exact Finset.prod_eq_single_of_mem i (by simp) (fun j _ hj ↦ by simp [g, hj])
+  rw [← this]
+  refine Integrable.fintype_prod_dep fun j ↦ ?_
+  by_cases h : j = i
+  · cases h; simpa [g]
+  · simpa [g, h] using integrable_const 1
 
-lemma integral_eval_pi {i : Fin d} {μ : Fin d → Measure ℝ} [∀ i, IsProbabilityMeasure (μ i)]
-    {f : ℝ → ℝ} (hf : Integrable f (μ i)) :
-    ∫ (a : Fin d → ℝ), f (a i) ∂Measure.pi μ = ∫ x, f x ∂μ i := by
-  sorry
+lemma integral_eval_pi {ι 𝕜 : Type*} [Fintype ι] [RCLike 𝕜] {X : ι → Type*} {i : ι}
+    {mX : ∀ i, MeasurableSpace (X i)} {μ : (i : ι) → Measure (X i)}
+    [∀ i, IsProbabilityMeasure (μ i)] {f : X i → 𝕜} :
+    ∫ (x : Π i, X i), f (x i) ∂Measure.pi μ = ∫ x, f x ∂μ i := by
+  classical
+  let g : Π i, X i → 𝕜 := fun j ↦ if h : j = i then h ▸ f else 1
+  have : (fun x ↦ ∏ j, g j (x j)) = fun (x : Π i, X i) ↦ f (x i) := by
+    ext x
+    rw [show f (x i) = g i (x i) by simp [g]]
+    exact Finset.prod_eq_single_of_mem i (by simp) (fun j _ hj ↦ by simp [g, hj])
+  rw [← this, integral_fintype_prod_eq_prod, show ∫ x, f x ∂μ i = ∫ x, g i x ∂μ i by simp [g]]
+  exact Finset.prod_eq_single_of_mem i (by simp) (fun j _ hj ↦ by simp [g, hj])
 
 lemma integral_id_stdGaussian : ∫ x, x ∂(stdGaussian E) = 0 := by
   rw [stdGaussian, integral_map _ (by fun_prop)]
@@ -58,11 +75,9 @@ lemma integral_id_stdGaussian : ∫ x, x ∂(stdGaussian E) = 0 := by
   refine Finset.sum_eq_zero fun i _ ↦ ?_
   have : (∫ (a : Fin (Module.finrank ℝ E) → ℝ), a i ∂Measure.pi fun x ↦ gaussianReal 0 1)
       = ∫ x, x ∂gaussianReal 0 1 := by
-    convert integral_eval_pi (i := i) ?_
+    convert integral_eval_pi (i := i)
     · rfl
     · infer_instance
-    · rw [← memLp_one_iff_integrable]
-      exact memLp_id_gaussianReal 1
   simp [integral_smul_const, this]
 
 lemma isCentered_stdGaussian : ∀ L : Dual ℝ E, (stdGaussian E)[L] = 0 := by
@@ -82,16 +97,101 @@ lemma isCentered_stdGaussian : ∀ L : Dual ℝ E, (stdGaussian E)[L] = 0 := by
   rw [integral_mul_const]
   have : (∫ (a : Fin (Module.finrank ℝ E) → ℝ), a i ∂Measure.pi fun x ↦ gaussianReal 0 1)
       = ∫ x, x ∂gaussianReal 0 1 := by
-    convert integral_eval_pi (i := i) ?_
+    convert integral_eval_pi (i := i)
     · rfl
     · infer_instance
-    · rw [← memLp_one_iff_integrable]
-      exact memLp_id_gaussianReal 1
   simp [this]
+
+variable {ι : Type*} [Fintype ι] {Ω : ι → Type*} {mΩ : ∀ i, MeasurableSpace (Ω i)}
+    {μ : (i : ι) → Measure (Ω i)} [∀ i, IsProbabilityMeasure (μ i)] {X : Π i, Ω i → ℝ}
+
+lemma measurePreserving_eval (i : ι) :
+    MeasurePreserving (Function.eval i) (Measure.pi μ) (μ i) := by
+  refine ⟨measurable_pi_apply i, ?_⟩
+  ext s hs
+  classical
+  rw [Measure.map_apply (measurable_pi_apply i) hs, ← Set.univ_pi_update_univ, Measure.pi_pi]
+  have : μ i s = (μ i) (Function.update (fun j ↦ Set.univ) i s i) := by simp
+  rw [this]
+  exact Finset.prod_eq_single_of_mem i (by simp) (fun j _ hj ↦ by simp [hj])
+
+lemma lol {ι : Type*} [Fintype ι] {Ω 𝒳 : ι → Type*} {mΩ : ∀ i, MeasurableSpace (Ω i)}
+    {m𝒳 : ∀ i, MeasurableSpace (𝒳 i)}
+    {μ : (i : ι) → Measure (Ω i)} [∀ i, IsProbabilityMeasure (μ i)] {X : Π i, Ω i → 𝒳 i}
+    (mX : ∀ i, Measurable (X i)) :
+    iIndepFun (fun i ω ↦ X i (ω i)) (Measure.pi μ) := by
+  apply @iIndepFun_iff_map_fun_eq_pi_map (Π i, Ω i) ι _ (Measure.pi μ) _ 𝒳 _
+    (fun i x ↦ X i (x i)) _ ?_ |>.2
+  · symm
+    apply Measure.pi_eq
+    intro s hs
+    rw [Measure.map_apply]
+    · have : (fun (ω : Π i, Ω i) i ↦ X i (ω i)) ⁻¹' (Set.univ.pi s) =
+          Set.univ.pi (fun i ↦ (X i) ⁻¹' (s i)) := by
+        ext x
+        simp
+      rw [this, Measure.pi_pi]
+      congr with i
+      rw [Measure.map_apply]
+      · change _ = (Measure.pi μ) (((X i) ∘ (fun x ↦ x i)) ⁻¹' s i)
+        rw [Set.preimage_comp, ← Measure.map_apply (measurable_pi_apply i),
+          (measurePreserving_eval i).map_eq]
+        · exact mX i (hs i)
+      · fun_prop
+      · exact hs i
+    · fun_prop
+    · exact MeasurableSet.univ_pi hs
+  · exact fun i ↦ Measurable.aemeasurable (by fun_prop)
+
+lemma lol' {ι : Type*} [Fintype ι] {Ω 𝒳 : ι → Type*} {mΩ : ∀ i, MeasurableSpace (Ω i)}
+    {m𝒳 : ∀ i, MeasurableSpace (𝒳 i)}
+    {μ : (i : ι) → Measure (Ω i)} [∀ i, IsProbabilityMeasure (μ i)] {X : Π i, Ω i → 𝒳 i}
+    (mX : ∀ i, AEMeasurable (X i) (μ i)) :
+    iIndepFun (fun i ω ↦ X i (ω i)) (Measure.pi μ) := by
+  have : iIndepFun (fun i ω ↦ (mX i).mk (X i) (ω i)) (Measure.pi μ) :=
+    lol (fun i ↦ (mX i).measurable_mk)
+  apply this.congr
+  intro i
+  change ((mX i).mk (X i)) ∘ Function.eval i =ᶠ[_] (X i) ∘ Function.eval i
+  apply ae_eq_comp
+  · exact (measurable_pi_apply i).aemeasurable
+  · rw [(measurePreserving_eval i).map_eq]
+    exact EventuallyEq.symm (AEMeasurable.ae_eq_mk (mX i))
+
+lemma variance_pi {ι : Type*} [Fintype ι] {Ω : ι → Type*} {mΩ : ∀ i, MeasurableSpace (Ω i)}
+    {μ : (i : ι) → Measure (Ω i)} [∀ i, IsProbabilityMeasure (μ i)] {X : (i : ι) → Ω i → ℝ}
+    (h : ∀ i, MemLp (X i) 2 (μ i)) :
+    Var[∑ i, fun ω ↦ X i (ω i); Measure.pi μ] = ∑ i, Var[X i; μ i] := by
+  classical
+  rw [IndepFun.variance_sum]
+  · congr with i
+    rw [variance_eq_integral, integral_eval_pi,
+      @integral_eval_pi ι _ _ _ Ω i _ μ _ (fun x : Ω i ↦ (X i x - ∫ y, X i y ∂μ i) ^ 2),
+      variance_eq_integral]
+    · exact (h i).aestronglyMeasurable.aemeasurable
+    · exact (h i).aestronglyMeasurable.aemeasurable.comp_quasiMeasurePreserving
+        (measurePreserving_eval i).quasiMeasurePreserving
+  · intro i hi
+    exact (h i).comp_measurePreserving (measurePreserving_eval i)
+  · rintro i - j - hij
+    refine @iIndepFun.indepFun (Π i, Ω i) ι _ (Measure.pi μ) (fun _ ↦ ℝ) _ (fun i x ↦ X i (x i)) ?_
+      i j hij
+    exact lol' fun i ↦ (h i).aestronglyMeasurable.aemeasurable
 
 lemma variance_dual_stdGaussian (L : Dual ℝ E) :
     Var[L; stdGaussian E] = ∑ i, L (stdOrthonormalBasis ℝ E i) ^ 2 := by
-  sorry
+  rw [stdGaussian, variance_map]
+  · have : L ∘ (fun x : Fin (Module.finrank ℝ E) → ℝ ↦ ∑ i, x i • stdOrthonormalBasis ℝ E i) =
+        ∑ i, (fun x : Fin (Module.finrank ℝ E) → ℝ ↦ L (stdOrthonormalBasis ℝ E i) * x i) := by
+      ext x; simp [mul_comm]
+    rw [this, variance_pi]
+    · change ∑ i, Var[fun x ↦ _ * (id x); gaussianReal 0 1] = _
+      simp_rw [variance_mul, variance_id_gaussianReal]
+      simp
+    · intro i
+      apply MemLp.const_mul
+      exact (isGaussian_gaussianReal 0 1).memLp_ _
+
 
 instance isGaussian_stdGaussian : IsGaussian (stdGaussian E) := by
   refine isGaussian_of_charFunDual_eq fun L ↦ ?_
