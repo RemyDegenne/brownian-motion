8f9d7bc6c315672c99c648c0ba949573b3c24350
Remy Degenne <remydegenne@gmail.com>
2025-06-17 07:32:44 +0200

    adapt Lean code

--- BrownianMotion/Continuity/IsKolmogorovProcess.lean
+++ BrownianMotion/Continuity/IsKolmogorovProcess.lean
@@ -307,6 +307,10 @@ section Together
 
 variable {c M : ℝ≥0} {d p q : ℝ} {J : Set T} {δ : ℝ≥0∞}
 
+noncomputable
+def Cp (d p q : ℝ) : ℝ≥0∞ :=
+  max (1 / ((2 ^ ((q - d) / p)) - 1) ^ p) (1 / (2 ^ (q - d) - 1))
+
 theorem finite_set_bound_of_edist_le (hJ : HasBoundedInternalCoveringNumber J c d)
     (hX : IsKolmogorovProcess X P p q M)
     (hd_pos : 0 < d) (hp_pos : 0 < p) (hdq_lt : d < q) (hδ : δ ≠ 0) :
@@ -314,7 +318,7 @@ theorem finite_set_bound_of_edist_le (hJ : HasBoundedInternalCoveringNumber J c 
       ≤ 4 ^ (p + 2 * q + 1) * M * δ ^ (q - d)
         * ((δ ^ d * Nat.log2 (internalCoveringNumber (δ / 4) J).toNat) ^ q
             * internalCoveringNumber (δ / 4) J
-          + c / ((2 ^ ((q - d) / p)) - 1) ^ p) := by
+          + c * Cp d p q) := by
   sorry
 
 lemma finite_set_bound_of_edist_le_of_le_diam (hJ : HasBoundedInternalCoveringNumber J c d)
@@ -324,7 +328,7 @@ lemma finite_set_bound_of_edist_le_of_le_diam (hJ : HasBoundedInternalCoveringNu
     ∫⁻ ω, ⨆ (s) (t) (_hs : s ∈ J) (_ht : t ∈ J) (_hd : edist s t ≤ δ), edist (X s ω) (X t ω) ^ p ∂P
       ≤ 4 ^ (p + 2 * q + 1) * M * c * δ ^ (q - d)
         * ((4 ^ d * ENNReal.ofReal (Real.logb 2 ((c : ℝ) * 4 ^ d * δ.toReal⁻¹ ^ d))) ^ q
-            + 1 / ((2 ^ ((q - d) / p)) - 1) ^ p) := by
+            + Cp d p q) := by
   sorry
 
 lemma finite_set_bound_of_edist_le_of_diam_le (hJ : HasBoundedInternalCoveringNumber J c d)
@@ -332,7 +336,7 @@ lemma finite_set_bound_of_edist_le_of_diam_le (hJ : HasBoundedInternalCoveringNu
     (hd_pos : 0 < d) (hp_pos : 0 < p) (hdq_lt : d < q)
     (hδ : δ ≠ 0) (hδ_le : EMetric.diam J ≤ δ / 4) :
     ∫⁻ ω, ⨆ (s) (t) (_hs : s ∈ J) (_ht : t ∈ J) (_hd : edist s t ≤ δ), edist (X s ω) (X t ω) ^ p ∂P
-      ≤ 4 ^ (p + 2 * q + 1) * M * c * δ ^ (q - d) / ((2 ^ ((q - d) / p)) - 1) ^ p := by
+      ≤ 4 ^ (p + 2 * q + 1) * M * c * δ ^ (q - d) * Cp d p q := by
   sorry
 
 lemma finite_set_bound_of_edist_le_bis (hJ : HasBoundedInternalCoveringNumber J c d)
@@ -341,7 +345,7 @@ lemma finite_set_bound_of_edist_le_bis (hJ : HasBoundedInternalCoveringNumber J 
     ∫⁻ ω, ⨆ (s) (t) (_hs : s ∈ J) (_ht : t ∈ J) (_hd : edist s t ≤ δ), edist (X s ω) (X t ω) ^ p ∂P
       ≤ 4 ^ (p + 2 * q + 1) * M * c * δ ^ (q - d)
         * ((4 ^ d * ENNReal.ofReal (Real.logb 2 ((c : ℝ) * 4 ^ d * δ.toReal⁻¹ ^ d))) ^ q
-            + 1 / ((2 ^ ((q - d) / p)) - 1) ^ p) := by
+            + Cp d p q) := by
   sorry
 
 end Together
--- BrownianMotion/Continuity/KolmogorovChentsov.lean
+++ BrownianMotion/Continuity/KolmogorovChentsov.lean
@@ -42,7 +42,7 @@ def constL (T : Type*) [PseudoEMetricSpace T] (c : ℝ≥0∞) (d p q β : ℝ) 
   4 ^ (p + 2 * q + 1) * c * (EMetric.diam (.univ : Set T) + 1) ^ (q - d)
   * ∑' k, 2 ^ (k * β * p + (-k + 1) * (q - d))
       * (4 ^ d * (ENNReal.ofReal (Real.logb 2 c.toReal + (k + 1) * d)) ^ q
-        + 1 / (2 ^ ((q - d) / p) - 1) ^ p)
+        + Cp d p q)
 
 lemma constL_lt_top (hc : c ≠ ∞) (hd_pos : 0 < d) (hp_pos : 0 < p) (hdq_lt : d < q)
     (hβ_pos : 0 < β) (hβ_lt : β < (q - d) / p) :
