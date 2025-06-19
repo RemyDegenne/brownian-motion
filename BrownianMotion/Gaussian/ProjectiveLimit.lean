3cbbe25968cb9cccd897cadb49b3704dfc610a68
Remy Degenne <remydegenne@gmail.com>
2025-06-19 14:31:02 +0200

    prove isGaussian_gaussianProjectiveFamily

--- BrownianMotion/Gaussian/ProjectiveLimit.lean
+++ BrownianMotion/Gaussian/ProjectiveLimit.lean
@@ -11,7 +11,7 @@ import BrownianMotion.Init
 
 -/
 
-open MeasureTheory
+open MeasureTheory NormedSpace
 open scoped ENNReal NNReal
 
 namespace ProbabilityTheory
@@ -39,10 +39,32 @@ def gaussianProjectiveFamily (I : Finset ℝ≥0) : Measure ((i : I) → ℝ) :=
   (gaussianProjectiveFamilyAux (fun i ↦ (I.equivFin).symm i)).map
     ((MeasurableEquiv.refl ℝ).arrowCongr' I.equivFin.symm)
 
+noncomputable
+def dualEuclideanSpaceFromDualSubtype (I : Finset ℝ≥0) (L : ({ x // x ∈ I } → ℝ) →L[ℝ] ℝ) :
+    Dual ℝ (EuclideanSpace ℝ (Fin I.card)) :=
+  LinearMap.toContinuousLinearMap
+    { toFun := L ∘ ((MeasurableEquiv.refl ℝ).arrowCongr' I.equivFin.symm)
+      map_add' x y := by
+        have : (x + y) ∘ I.equivFin = x ∘ I.equivFin + y ∘ I.equivFin := by ext; simp
+        simp [MeasurableEquiv.arrowCongr', Equiv.arrowCongr', Equiv.arrowCongr,
+          MeasurableEquiv.refl, this]
+      map_smul' c x := by
+        have : (c • x) ∘ I.equivFin = c • (x ∘ I.equivFin) := by ext; simp
+        simp [MeasurableEquiv.arrowCongr', Equiv.arrowCongr', Equiv.arrowCongr,
+          MeasurableEquiv.refl, this] }
+
+lemma coe_dualEuclideanSpaceFromDualSubtype (I : Finset ℝ≥0) (L : ({ x // x ∈ I } → ℝ) →L[ℝ] ℝ) :
+    (dualEuclideanSpaceFromDualSubtype I L : EuclideanSpace ℝ (Fin I.card) → ℝ)
+     = L ∘ ((MeasurableEquiv.refl ℝ).arrowCongr' I.equivFin.symm) := rfl
+
 instance isGaussian_gaussianProjectiveFamily (I : Finset ℝ≥0) :
-    IsGaussian (gaussianProjectiveFamily I) := by
-  unfold gaussianProjectiveFamily
-  sorry
+    IsGaussian (gaussianProjectiveFamily I) where
+  map_eq_gaussianReal L := by
+    unfold gaussianProjectiveFamily
+    rw [Measure.map_map (by fun_prop) (by fun_prop), variance_map (by fun_prop) (by fun_prop),
+      integral_map (by fun_prop) (by fun_prop), ← coe_dualEuclideanSpaceFromDualSubtype,
+      IsGaussian.map_eq_gaussianReal (dualEuclideanSpaceFromDualSubtype I L)]
+    congr
 
 lemma isProjectiveMeasureFamily_gaussianProjectiveFamily :
     IsProjectiveMeasureFamily (α := fun _ ↦ ℝ) gaussianProjectiveFamily := by
