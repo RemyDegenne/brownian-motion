b95794fc21e51cf781627afd7cc872ad7a5d72a5
DavidLedvinka <david.ledvinka@mail.utoronto.ca>
2025-06-19 17:23:25 -0400

    finished rough

--- BrownianMotion/Continuity/KolmogorovChentsov.lean
+++ BrownianMotion/Continuity/KolmogorovChentsov.lean
@@ -16,57 +16,61 @@ open scoped ENNReal NNReal
 
 namespace ProbabilityTheory
 
-variable {T Ω E : Type*} [PseudoEMetricSpace T] {mΩ : MeasurableSpace Ω}
-  [PseudoEMetricSpace E] [MeasurableSpace E] [BorelSpace E]
+variable {T Ω E : Type*} [EMetricSpace T] {mΩ : MeasurableSpace Ω}
+  [PseudoEMetricSpace E] [SecondCountableTopology E] [MeasurableSpace E] [BorelSpace E]
   {X : T → Ω → E}
   {c : ℝ≥0∞} {d p q : ℝ} {M β : ℝ≥0}
   {P : Measure Ω}
 
 lemma lintegral_div_edist_le_sum_integral_edist_le (hT : EMetric.diam (Set.univ : Set T) < ∞)
-<<<<<<< HEAD
-    (hX : ∀ t, AEMeasurable (X t) P) (hβ_pos : 0 < β) (hp_pos : 0 < p) {J : Set T} :
-    ∫⁻ ω, ⨆ (s) (t) (hs : s ∈ J) (ht : t ∈ J), edist (X s ω) (X t ω) ^ p / edist s t ^ (β * p) ∂P
-      ≤ ∑' (k : ℕ), 2 ^ (k * β * p)
-=======
     (hX : ∀ t, AEMeasurable (X t) P)
-    (hβ_pos : 0 < β) (hβ_lt : β < (q - d) / p)
-    {J : Set T} (hJ : J.Finite) :
+    (hβ_pos : 0 < β) (hp_pos : 0 < p) {J : Set T} (hJ : Countable J) :
     ∫⁻ ω, ⨆ (s : J) (t : J), edist (X s ω) (X t ω) ^ p / edist s t ^ (β * p) ∂P
-      ≤ ∑' k, 2 ^ (k * β * p)
->>>>>>> 8ab069a5ca8ab00b0fe08348041f5bb5e6268dd2
-          * ∫⁻ ω, ⨆ (s) (t) (hs : s ∈ J) (ht : t ∈ J)
+      ≤ ∑' (k : ℕ), 2 ^ (k * β * p)
+          * ∫⁻ ω, ⨆ (s : J) (t : J)
             (_he : edist s t ≤ 2 * 2⁻¹ ^ k * (EMetric.diam (.univ : Set T) + 1)),
             edist (X s ω) (X t ω) ^p ∂P := by
   let η k := 2⁻¹ ^ k * (EMetric.diam (Set.univ : Set T) + 1)
   have hη_ge (k : ℕ) : 2⁻¹ ^ (k : ℝ) ≤ η k := by simp [η, mul_add]
-  have h : ⋃ k, Set.Icc (η k) (2 * η k) = Set.Icc 0 (EMetric.diam (Set.univ : Set T) + 1) := by
-    sorry
-  conv in 2 ^ _ * _ => rw [← lintegral_const_mul _ (by sorry)]
-  rw [← lintegral_tsum (by sorry)]
+  have hη_succ (k : ℕ) : η (k + 1) = 2⁻¹ * η k := by simp [η, pow_add, mul_assoc, mul_comm]
+  have hη_lim : Filter.Tendsto η Filter.atTop (nhds 0) := by
+    unfold η
+    rw [← zero_mul (EMetric.diam (Set.univ : Set T) + 1)]
+    apply ENNReal.Tendsto.mul_const (ENNReal.tendsto_pow_atTop_nhds_zero_of_lt_one (by norm_num))
+    simp [← lt_top_iff_ne_top, hT]
+  conv in 2 ^ _ * _ => rw [← lintegral_const_mul' _ _ (by simp)]
+  rw [← lintegral_tsum (by fun_prop)]
   refine lintegral_mono (fun ω ↦ ?_)
-  rw [iSup_le_iff]; intro s
-  rw [iSup_le_iff]; intro t
-  rw [iSup_le_iff]; intro hs
-  rw [iSup_le_iff]; intro ht
+  rw [iSup_le_iff]; rintro ⟨s, hs⟩
+  rw [iSup_le_iff]; intro ⟨t, ht⟩
+  wlog hst : s ≠ t
+  · simp [not_ne_iff.mp hst, edist_self, ge_iff_le, ENNReal.zero_rpow_of_pos (by positivity)]
   obtain ⟨k, lb, ub⟩ : ∃ k, (η k ≤ edist s t) ∧ (edist s t ≤ 2 * η k) := by
-    simp_rw [← Set.mem_Icc, ← Set.mem_iUnion, h, Set.mem_Icc]
-    refine ⟨zero_le _, ?_⟩
-    exact le_add_right <| EMetric.edist_le_diam_of_mem (Set.mem_univ _) (Set.mem_univ _)
+    have hη_dist : ∃ k, η k ≤ edist s t :=
+      (Filter.Eventually.exists (Filter.Tendsto.eventually_le_const (edist_pos.mpr hst) hη_lim))
+    refine ⟨Nat.find hη_dist, Nat.find_spec hη_dist, ?_⟩
+    match hk : Nat.find hη_dist with
+    | 0 =>
+        apply le_trans (EMetric.edist_le_diam_of_mem (Set.mem_univ _) (Set.mem_univ _))
+        simp [η]
+        exact le_mul_of_one_le_of_le (by norm_num) (le_add_right (le_refl _))
+    | k + 1 =>
+        rw [hη_succ k, ← mul_assoc, ENNReal.mul_inv_cancel (by norm_num) (by norm_num), one_mul]
+        refine le_of_not_ge (Nat.find_min hη_dist ?_)
+        simp [hk]
   refine le_trans ?_ (Summable.le_tsum (ENNReal.summable) k (fun _ _ ↦ zero_le _))
   rw [ENNReal.div_eq_inv_mul]
   refine mul_le_mul ?_ ?_ (zero_le _) (zero_le _)
   · rw [ENNReal.inv_le_iff_inv_le, ← ENNReal.inv_rpow, mul_assoc, ENNReal.rpow_mul,
       ENNReal.rpow_le_rpow_iff (by positivity)]
     exact le_trans (hη_ge k) lb
-
-
-
-
-
-
-
-
-
+  apply le_trans
+  · apply le_iSup (fun (_ :edist s t ≤ 2 * 2⁻¹ ^ k * (EMetric.diam (.univ : Set T) + 1))
+      ↦ edist (X s ω) (X t ω) ^ p)
+    rwa [mul_assoc]
+  apply le_iSup_of_le (i := ⟨s, hs⟩)
+  apply le_iSup_of_le (i := ⟨t, ht⟩)
+  exact le_refl _
 
 noncomputable
 -- the `max 0 ...` in the blueprint is performed by `ENNReal.ofReal` here
