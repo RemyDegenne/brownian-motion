a4546b1318bd8343f88c735d564a4314525e0028
Etienne Marion <baguettes_casino0c@icloud.com>
2025-06-26 22:54:08 +0200

    min imports

--- BrownianMotion/Gaussian/BrownianMotion.lean
+++ BrownianMotion/Gaussian/BrownianMotion.lean
@@ -3,12 +3,11 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import BrownianMotion.Auxiliary.Nat
 import BrownianMotion.Auxiliary.NNReal
+import BrownianMotion.Continuity.KolmogorovChentsov
 import BrownianMotion.Gaussian.GaussianProcess
 import BrownianMotion.Gaussian.Moment
 import BrownianMotion.Gaussian.ProjectiveLimit
-import BrownianMotion.Continuity.KolmogorovChentsov
 
 /-!
 # Brownian motion
