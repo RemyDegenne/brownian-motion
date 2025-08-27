4f1f821a3337df18c3d2c311249b2340c09e6e21
Lorenzo Luccioli <lorenzoluccioli@gmail.com>
2025-08-27 13:28:51 +0200

    apply changes

---
+++ BrownianMotion/Debut/Approximation.lean
@@ -0,0 +1,333 @@
+/-
+Copyright (c) 2025 Lorenzo Luccioli. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Lorenzo Luccioli
+-/
+
+import Mathlib.Order.CompletePartialOrder
+import Mathlib.Probability.Process.Filtration
+
+/-!
+This file contains the definitions and some theorems about t-approximable sets, which are needed
+in the proof of the Debut Theorem.
+-/
+
+open Filter Order TopologicalSpace
+
+open scoped MeasureTheory NNReal ENNReal Topology
+
+
+namespace MeasureTheory
+
+variable {T Ω : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω} [Preorder T] [TopologicalSpace T]
+variable {f : Filtration T mΩ} {t : T}
+
+/- NB: we definitely need more hypotheses on T, for example we may need `Set.Iic t` to be compact
+(what would be a good way to impose this? It seems that there exists the class `isClosed_Iic`
+that does exactly that, maybe it is enough). Anyways, let us put them only where they are needed. -/
+
+section 𝒦_sets
+
+/- TODO: Can this definition be written more clearly, I wanted to write something like
+`{ A ×ˢ C | (A ⊆ Set.Iic t) ∧ IsCompact A ∧ MeasurableSet[f t] C }`, but it gives me invalid
+pattern error, I'm not sure why. I also tried with an inductive definition, maybe it is a bit more
+clean, for now I am keeping that one. -/
+
+/- NB: a set in lean (s : Set α) is defined as a function s : α → Prop, and s a for a : α just
+means that a ∈ s. However I think this notation should be mostly hidden from the user, so is it ok
+to leave the definitions mentioning this explicitely? -/
+
+/--  `𝓚₀(t)` is the collection of subsets of `[0, t] × Ω` of the form `A × C`, where `A` is
+compact and `C` is (f t)-measurable. -/
+inductive 𝓚₀ (f : Filtration T mΩ) (t : T) : Set (Set (T × Ω)) where
+  | prod {K C} (hA_subs : K ⊆ Set.Iic t) (hA : IsCompact K) (hM : MeasurableSet[f t] C) :
+    𝓚₀ f t (K ×ˢ C)
+
+@[simp]
+lemma empty_mem_𝓚₀ (f : Filtration T mΩ) (t : T) : ∅ ∈ 𝓚₀ f t := by
+  convert 𝓚₀.prod (Set.empty_subset _) isCompact_empty (@MeasurableSet.empty _ (f t))
+  exact Set.prod_empty.symm
+
+lemma mem_𝓚₀_iff (f : Filtration T mΩ) (t : T) (B : Set (T × Ω)) :
+    B ∈ 𝓚₀ f t ↔ ∃ K C, B = K ×ˢ C ∧ K ⊆ Set.Iic t ∧ IsCompact K ∧ MeasurableSet[f t] C := by
+  -- easy, just unfold the definition correctly (how do you unfold an inductive definition?)
+  sorry
+
+lemma subset_Iic_of_mem_𝓚₀ {B : Set (T × Ω)} (hB : B ∈ 𝓚₀ f t) :
+    B ⊆ Set.Iic t ×ˢ .univ := by
+  have ⟨A, C, hB_eq, hA_subs, hA, hC⟩ := (mem_𝓚₀_iff ..).mp hB
+  exact hB_eq ▸ Set.prod_mono hA_subs (Set.subset_univ _)
+
+/-- If `B ∈ 𝓚₀(t)`, then its projetion over `Ω` is (f t)-measurable. -/
+lemma measurableSet_snd_of_mem_𝓚₀ {B : Set (T × Ω)} (hB : B ∈ 𝓚₀ f t) :
+    MeasurableSet[f t] (Prod.snd '' B) := by
+  -- easy, divide the case K = ∅
+  sorry
+
+/-- `𝓚(t)` is the collection of finite unions of sets in `𝓚₀(t)`. -/
+inductive 𝓚 (f : Filtration T mΩ) (t : T) : Set (Set (T × Ω)) where
+  | base {B : Set (T × Ω)} (hB : B ∈ 𝓚₀ f t) : 𝓚 f t B
+  | union {B B' : Set (T × Ω)} (hB : B ∈ 𝓚 f t) (hB' : B' ∈ 𝓚 f t) :
+      𝓚 f t (B ∪ B')
+
+lemma 𝓚₀_subset_𝓚 (f : Filtration T mΩ) (t : T) : 𝓚₀ f t ⊆ 𝓚 f t := fun _ hB ↦ 𝓚.base hB
+
+lemma mem_𝓚_iff (f : Filtration T mΩ) (t : T) (B : Set (T × Ω)) :
+    B ∈ 𝓚 f t ↔ ∃ s : Finset (Set (T × Ω)),
+      (s : Set _) ⊆ 𝓚₀ f t ∧ B = ⋃ x ∈ s, x := by
+  -- farily easy
+  sorry
+
+lemma subset_Iic_of_mem_𝓚 {B : Set (T × Ω)} (hB : B ∈ 𝓚 f t) :
+    B ⊆ Set.Iic t ×ˢ .univ := by
+  induction hB with
+  | base hB => exact subset_Iic_of_mem_𝓚₀ hB
+  | union _ _ hB hB' => exact Set.union_subset hB hB'
+
+/-- `𝓚(t)` is closed under union. -/
+lemma union_mem_𝓚 {f : Filtration T mΩ} {t : T}
+    {B B' : Set (T × Ω)} (hB : B ∈ 𝓚 f t) (hB' : B' ∈ 𝓚 f t) : B ∪ B' ∈ 𝓚 f t := by
+  -- easy
+  sorry
+
+/-- If `B ∈ 𝓚(t)`, then its projection over `Ω` is (f t)-measurable. -/
+lemma measurableSet_snd_of_mem_𝓚 {B : Set (T × Ω)} (hB : B ∈ 𝓚 f t) :
+    MeasurableSet[f t] (Prod.snd '' B) := by
+  /- should be easy, just use `measurableSet_snd_of_mem_𝓚₀` and the fact that the union of snd is
+  snd of the union (`Set.image_union`) and then the fact that union of measurable is measurable -/
+  sorry
+
+inductive 𝓚δ (f : Filtration T mΩ) (t : T) : Set (Set (T × Ω)) where
+  | iInter {ℬ : Set (Set (T × Ω))} (h_nonempty : ℬ.Nonempty) (h_subs : ℬ ⊆ 𝓚 f t)
+    (h_count : Countable ℬ) : 𝓚δ f t (⋂ B ∈ ℬ, B)
+
+lemma subset_Iic_of_mem_𝓚δ {B : Set (T × Ω)} (hB : B ∈ 𝓚δ f t) :
+    B ⊆ Set.Iic t ×ˢ .univ := by
+  induction hB with
+  | iInter h_nonempty h_subs h_count =>
+    have ⟨B, hB⟩ := h_nonempty
+    exact Set.iInter₂_subset_of_subset B hB (subset_Iic_of_mem_𝓚 (h_subs hB))
+
+/-- `𝓚δ(t)` is closed under union. -/
+lemma union_mem_𝓚δ {f : Filtration T mΩ} {t : T}
+    {B B' : Set (T × Ω)} (hB : B ∈ 𝓚δ f t) (hB' : B' ∈ 𝓚δ f t) : B ∪ B' ∈ 𝓚δ f t := by
+  -- easy, you can use `union_mem_𝓚`, `Set.iInter_union` and `Set.union_iInter`
+  sorry
+
+/- TODO: check that this is provable even without the hypothesis that `B := ⋂ B_n ⊆ 𝒦δ`, I'm not
+completely sure. If it is not possible to prove it like this, then just add the hypothesis
+`⋂ B_n ⊆ 𝒦δ`. -/
+/-- In `𝓚δ`, the projection over `Ω` and countable intersections commute. -/
+lemma iInf_snd_eq_snd_iInf_of_mem_𝓚δ {t : T} {ι : Type*} [Countable ι]
+    {ℬ : ι → Set (T × Ω)} (hℬ : ∀ i, ℬ i ∈ 𝓚δ f t) :
+    ⋂ i, Prod.snd '' ℬ i = Prod.snd '' (⋂ i, ℬ i) := by
+  -- see proof in the blueprint
+
+  /- the intersection of the `S(Bn)` is compact since each of them is compact, why? maybe we can
+  use the fact that they are all contained in the compact set `Set.Iic t` and they are closed (why
+  are they closed?? it should be fairly easy to show it starting from the claim that `S(A)` is cpct
+  for `A ∈ 𝒦₀`)
+  maybe use `IsCompact.nonempty_iInter_of_sequence_nonempty_isCompact_isClosed` for the second case
+  to show that the intersection is nonempty and reach the contradiction -/
+  sorry
+
+/-- If `B ∈ 𝓚δ(t)`, then its projetion over `Ω` is (f t)-measurable. -/
+lemma measurableSet_snd_of_mem_𝓚δ {B : Set (T × Ω)} (hB : B ∈ 𝓚δ f t) :
+    MeasurableSet[f t] (Prod.snd '' B) := by
+  -- use `iInf_snd_eq_snd_iInf_of_mem_𝓚δ`, `measurableSet_snd_of_mem_𝓚` and the definition of `𝒦δ`
+  sorry
+
+end 𝒦_sets
+
+section 𝓛_sets
+
+/-- `𝓛₀(X)` is the collection of sets of the form `A ×ˢ B`, where `A : Set X` is compact and
+`B ∈ 𝓚 f t`. -/
+inductive 𝓛₀ (X : Type*) [TopologicalSpace X] (f : Filtration T mΩ) (t : T) :
+    Set (Set (X × (T × Ω))) where
+  | prod {A B} (hA : IsCompact A) (hB : B ∈ 𝓚 f t) : 𝓛₀ X f t (A ×ˢ B)
+
+/-- `𝓛₁(X)` is the collection of finite unions of sets in `𝓛₀(X)`. -/
+inductive 𝓛₁ (X : Type*) [TopologicalSpace X] (f : Filtration T mΩ) (t : T) :
+    Set (Set (X × (T × Ω))) where
+  | base {L} (hL : L ∈ 𝓛₀ X f t) : 𝓛₁ X f t L
+  | union {L L'} (hL : L ∈ 𝓛₁ X f t) (hL' : L' ∈ 𝓛₁ X f t) : 𝓛₁ X f t (L ∪ L')
+
+/-- `𝓛(X)` is the collection of intersections of countable decreasing sequences in `𝓛₁(X)`. -/
+inductive 𝓛 (X : Type*) [TopologicalSpace X] (f : Filtration T mΩ) (t : T) :
+    Set (Set (X × (T × Ω))) where
+  | iInter {L : ℕ → Set (X × (T × Ω))} (h_anti : Antitone L) (hL : ∀ n, L n ∈ 𝓛₁ X f t) :
+      𝓛 X f t (⋂ n, L n)
+
+/-- `𝓛σ(X)` is the collection of unions of countable increasing sequences in `𝓛(X)`. -/
+inductive 𝓛σ (X : Type*) [TopologicalSpace X] (f : Filtration T mΩ) (t : T) :
+    Set (Set (X × (T × Ω))) where
+  | iUnion {L : ℕ → Set (X × (T × Ω))} (h_incr : Monotone L) (hL : ∀ n, L n ∈ 𝓛 X f t) :
+      𝓛σ X f t (⋃ n, L n)
+
+/-- `𝓛σδ(X)` is the collection of intersections of countable decreasing sequences in `𝓛σ(X)`. -/
+inductive 𝓛σδ (X : Type*) [TopologicalSpace X] (f : Filtration T mΩ) (t : T) :
+    Set (Set (X × (T × Ω))) where
+  | iInter {L : ℕ → Set (X × (T × Ω))} (h_anti : Antitone L) (hL : ∀ n, L n ∈ 𝓛σ X f t) :
+      𝓛σδ X f t (⋂ n, L n)
+
+/- is this the best way to state that there exists a compact T2 topological space `X`? is there a
+more compact way, without having to write `∃` 4 times? -/
+/-- If `A` is measurable with respect to the product sigma algebra of the Borel sigma algebra on
+`T` and `f t`, then there exists a compact Hausdorff space `X` and a set `B ∈ 𝓛σδ X f t` such that
+`A` is the projection of `B` on `T × Ω`. -/
+lemma exists_mem_𝓛σδ_of_measurableSet {mT : MeasurableSpace T} [BorelSpace T]
+    {A : Set (T × Ω)} (hA_subs : A ⊆ Set.Iic t ×ˢ .univ) (hA : MeasurableSet[mT.prod (f t)] A) :
+    ∃ X : Type*, ∃ _ : TopologicalSpace X, ∃ _ : CompactSpace X, ∃ _ : T2Space X,
+      ∃ B ∈ 𝓛σδ X f t, A = Prod.snd '' B := by
+  /- See the proof of lemma A.2 in the paper. This will need some work and probably some
+  auxiliary lemmas.
+
+  possible strategy: define the class M, then prove as a lemma that it is a monotone class, prove
+  that it contains K(t) then it contains I(t) (which is an algebra), then use the monotone class
+  theorem inside the proof of this lemma to conclude.
+  we will need the monotone class theorem, a quick search in mathlib does not give me any
+  satisfactory result. maybe I need to write a separate file for this and prove this theorem myself,
+  but I have no idea how difficult it is. Maybe there is something similar already partially
+  implemented, see this Zulip message:
+  https://leanprover.zulipchat.com/#narrow/channel/113489-new-members/topic/Proof.3A.20.20isField.20ss.20.E2.88.A7.20isMonoClass.20ss.20.E2.86.94.20isSigmaField.20ss.20.3F/near/448825855 -/
+  sorry
+
+
+end 𝓛_sets
+
+/- Is this the right way to define an approximable set? In this way we have the approximation ready
+to use, without having to use .choose, but maybe it makes sense to have a Prop instead? Also, do we
+actually need this to be explicit? I'm not sure, but for now I leave it as a structure -/
+
+/- does it make sense to have `ε : ENNReal`, or would it be better to have `ε : ℝ` or `ε : ℝ≥0`?
+having `ε : ENNReal` has the advantage that we dont have coercions inside the formula, but maybe
+it does not affect much? for now I leave it as `ENNReal`, but if it gives any trouble we can
+change it. -/
+/-- An approximation of `A ⊆ Set.Iic t ×ˢ .univ` is a sequence of sets `B ε ⊆ A` for every `ε > 0`
+such that `B ε ∈ 𝓚δ f t`, and the projection of `B ε` on `Ω` approximates `A` in measure
+as `ε` tends to `0`. -/
+structure Approximation (f : Filtration T mΩ) (P : Measure Ω) (t : T) (A : Set (T × Ω)) where
+  B : ℝ≥0∞ → Set (T × Ω)
+  A_subset : A ⊆ Set.Iic t ×ˢ .univ
+  B_mem : ∀ ε > 0, B ε ∈ 𝓚δ f t
+  B_subset_A : ∀ ε > 0, B ε ⊆ A
+  le : ∀ ε > 0, P (Prod.snd '' A) ≤ P (Prod.snd '' B ε) + ε
+
+namespace Approximation
+
+variable {P : Measure Ω} {A : Set (T × Ω)}
+
+/-- A set `A ∈ 𝓚δ(t)` is approximable (the approximation is the set itself). -/
+def of_mem_𝓚δ (P : Measure Ω) (hA : A ∈ 𝓚δ f t) : Approximation f P t A where
+  B := fun _ ↦ A
+  A_subset := subset_Iic_of_mem_𝓚δ hA
+  B_mem := fun _ _ ↦ hA
+  B_subset_A := fun _ _ _ a ↦ a
+  le := fun _ _ ↦ le_self_add
+
+/-- Given an approximation `𝒜`, then `𝒜.B' n` is the union of `B 1, B (1/2), ..., B (1/n)`.
+This gives us an increasing approximating sequence. -/
+def B' (𝒜 : Approximation f P t A) : ℕ → Set (T × Ω) :=
+  fun n ↦ ⋃ m ∈ Finset.Icc 1 n, 𝒜.B (1 / m)
+
+@[simp]
+lemma B'_zero (𝒜 : Approximation f P t A) : 𝒜.B' 0 = ∅ := by
+  simp [B']
+
+lemma B'_succ (𝒜 : Approximation f P t A) (n : ℕ) :
+    𝒜.B' (n + 1) = 𝒜.B' n ∪ 𝒜.B (1 / (n + 1)) := by
+  have : Finset.Icc 1 (n + 1) = Finset.Icc 1 n ∪ {n + 1} := by
+    ext x
+    simp only [Finset.mem_Icc, Finset.mem_union, Finset.mem_singleton]
+    omega
+  rw [B', this, B', Finset.set_biUnion_union]
+  simp
+
+lemma B_subset_B' (𝒜 : Approximation f P t A) (n : ℕ) [NeZero n] :
+    𝒜.B (1 / n) ⊆ 𝒜.B' n := by
+  -- easy, use the definition of B'
+  sorry
+
+lemma B'_mem (𝒜 : Approximation f P t A) (n : ℕ) : 𝒜.B' n ∈ 𝓚δ f t := by
+  -- easy, use the definition of B', B_mem and the fact that 𝓚δ is closed under union
+  sorry
+
+lemma B'_subset_A (𝒜 : Approximation f P t A) (n : ℕ) : 𝒜.B' n ⊆ A := by
+  -- easy, use the definition of B' and B_subset_A
+  sorry
+
+lemma le' (𝒜 : Approximation f P t A) (n : ℕ) :
+    P (Prod.snd '' A) ≤ P (Prod.snd '' 𝒜.B' n) + 1 / n := by
+  -- easy, use the definition of B', le and the fact that B (1 / n) ⊆ B' n
+  sorry
+
+lemma B'_mono (𝒜 : Approximation f P t A) : Monotone 𝒜.B' := by
+  -- easy, use the definition of B'
+  sorry
+
+lemma measurableSet_snd_iUnion_B' (𝒜 : Approximation f P t A) :
+    MeasurableSet[f t] (Prod.snd '' (⋃ n, 𝒜.B' n)) := by
+  /- easy, use `measurableSet_snd_of_mem_𝓚δ`, the fact that a union of projections is the
+  projection of the union and the fact that a countable union of measurable sets is measurable -/
+  sorry
+
+lemma measure_snd_iUnion_B' (𝒜 : Approximation f P t A) :
+    P (Prod.snd '' (⋃ n, 𝒜.B' n)) = P (Prod.snd '' A) := by
+  /- see part of the proof of `lem:measurable_of_t_approx` in the blueprint. The idea is: let `π` be
+  the projection over `Ω`, `P(π(B)) = lim P(π(B_n)) = P(π(A))` -/
+  sorry
+
+lemma measure_snd_diff [IsFiniteMeasure P] (𝒜 : Approximation f P t A) :
+    P (Prod.snd '' A \ Prod.snd '' (⋃ n, 𝒜.B' n)) = 0 := by
+  /- easy, use `MeasureTheory.measure_diff` and the fact that `π(B) ⊆ π(A)` to say that
+  `P(π(A) \ π(B)) = 0`. -/
+  sorry
+
+-- we probably need a complete measure space for this lemma to hold.
+/-- If there exists an approximation of `A`, then the projection of `A` over `Ω` is measurable
+with respect to `f t`. -/
+lemma measurableSet_snd (𝒜 : Approximation f P t A) :
+    MeasurableSet[f t] (Prod.snd '' A) := by
+  /- easy, use the completeness and `measure_snd_diff` to get measurability for `(π(A) \ π(B))`,
+  then measurableSet_snd_iUnion_B' to get measurability for `π(B)`, and finally use the fact that
+  `π(A) = (π(A) \ π(B)) ∪ π(B)`. -/
+  sorry
+
+/- maybe it is better to state this with an inf instead of a limit? or maybe it is better to state
+it as in the paper, i.e. `∀ ε > 0, ∃ B ∈ 𝓚δ f t, P (Prod.snd '' A \ Prod.snd '' B) < ε` ? In any
+case it should be fairly easy to go from one to the other. -/
+lemma tendsto_measure_diff_B' (𝒜 : Approximation f P t A) :
+    Tendsto (fun n ↦ P (Prod.snd '' A \ Prod.snd '' 𝒜.B' n)) atTop (𝓝 0) := by
+  /- check the proof in the blueprint, much of the needed infrastructure is already formalized just
+  above this.-/
+  sorry
+
+/- Question: Is `A ⊆ Set.Iic t ×ˢ .univ ∧ MeasurableSet[mT.prod (f t)] A` equivalent to
+`A ∈ 𝓑 (Set.Iic t) × (f i)` (the last one is not syntactically correct in lean, with that I mean
+that A is measurable wrt the product σ algebra of the borel σ algebra induced by T on Set.Iic t and
+the σ alg on Ω, the problem with writing this explicitely is that we should first project A into
+`Set.Iic t ×ˢ Ω` and I fear that is not going to be very clean and usable). I'm pretty sure that if
+`A ⊆ Set.Iic t ×ˢ .univ` then it is just equivalent to being measurable wrt the global product σ
+algebra, so for now I'm going to formalize it that way, but it would be good if someone double
+checks that. -/
+
+-- is there a notation for `mT.prod (f t)`? `mT × (f t)` does not work
+/- how much is it needed that we take the Borel σ algebra on `T`? Take this into consideration when
+writing the proof, maybe we can relax this hypothesis. -/
+-- this is the main theorem of this file.
+/-- If `A` is measurable with respect to the product sigma algebra of the Borel sigma algebra on
+`T` and `f t`, then it is approximable. -/
+def of_mem_prod_borel {mT : MeasurableSpace T} [BorelSpace T] (P : Measure Ω)
+    (hA_subs : A ⊆ Set.Iic t ×ˢ .univ) (hA : MeasurableSet[mT.prod (f t)] A) :
+    Approximation f P t A where
+  -- See the proof in the blueprint
+  B := sorry
+  A_subset := sorry
+  B_mem := sorry
+  B_subset_A := sorry
+  le := sorry
+
+end Approximation
+
+end MeasureTheory
---
+++ BrownianMotion/Debut/Basic.lean
@@ -0,0 +1,222 @@
+/-
+Copyright (c) 2025 Lorenzo Luccioli. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Lorenzo Luccioli
+-/
+
+import Mathlib.Order.CompletePartialOrder
+import Mathlib.Probability.Process.HittingTime
+import BrownianMotion.Debut.Approximation
+
+/-!
+This file contains the basic definitions and properties of the debut of a set.
+
+
+## Implementation notes
+
+Following the implementation of hitting times in `Mathlib.Probability.Process.HittingTime`, we
+bound the debut time by an upper and lower bound to ensure that it is well-defined even when the
+set is empty or unbounded.
+This allows us to define the debut for an index space without a top element, such as `ℕ` or `ℝ`.
+The standard definition is recovered in the case of complete lattices, taking the bounds to
+be `⊥` and `⊤`.
+
+-/
+
+namespace MeasureTheory
+
+variable {Ω ι : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω}
+
+open scoped Classical in
+/-- The debut of a set `E ⊆ T × Ω` is the random variable that gives the smallest `t` such that
+`(t, ω) ∈ E` for a given `ω`. -/
+noncomputable def Debut [Preorder ι] [InfSet ι] (E : Set (ι × Ω)) (n m : ι) : Ω → ι :=
+  fun ω ↦ if ∃ t ∈ Set.Icc n m, (t, ω) ∈ E then
+    sInf {t ∈ Set.Icc n m | (t, ω) ∈ E} else m
+
+open scoped Classical in
+lemma debut_def [Preorder ι] [InfSet ι] (E : Set (ι × Ω)) (n m : ι) :
+    Debut E n m = fun ω ↦ if ∃ t ∈ Set.Icc n m, (t, ω) ∈ E then
+      sInf {t ∈ Set.Icc n m | (t, ω) ∈ E} else m := rfl
+
+-- TODO: prove that this def is equiv to the hitting time of an indicator function of E, when it hits [1,∞]
+
+namespace Debut
+
+variable [ConditionallyCompleteLinearOrder ι] (n m : ι)
+
+/-- The debut of the empty set is the constant function that returns `m`. -/
+@[simp]
+lemma empty : Debut (∅ : Set (ι × Ω)) n m = fun _ ↦ m := by
+  unfold Debut
+  simp
+
+@[simp]
+lemma univ [Nonempty Ω] : Debut (Set.univ : Set (ι × Ω)) n m = fun _ ↦ if n ≤ m then n else m := by
+  unfold Debut
+  split_ifs with h
+  · ext ω
+    rw [if_pos ⟨n, Set.left_mem_Icc.mpr h, Set.mem_univ _⟩]
+    simpa only [Set.mem_univ, and_true] using csInf_Icc h
+  · simp [h]
+
+open scoped Classical in
+@[simp]
+lemma prod (I : Set ι) (A : Set Ω) :
+    Debut (I ×ˢ A) n m = fun ω ↦ if Set.Icc n m ∩ I ≠ ∅ then
+        if ω ∈ A then sInf (Set.Icc n m ∩ I) else m
+      else m := by
+  ext ω
+  split_ifs with hI hω
+  · simp only [Debut, Set.mem_Icc, Set.mem_prod, hω, and_true]
+    rw [if_pos (by exact Set.nonempty_iff_ne_empty.mpr hI)]
+    congr
+  · simp [Debut, hω]
+  · simp only [ne_eq, Decidable.not_not] at hI
+    refine if_neg ?_
+    simp only [Set.mem_Icc, Set.mem_prod, not_exists, not_and, and_imp]
+    exact fun i hni him hiI _ ↦ Set.notMem_empty i (hI ▸ ⟨⟨hni, him⟩, hiI⟩)
+
+open scoped Classical in
+@[simp]
+lemma prod_univ (I : Set ι) :
+    Debut (I ×ˢ (.univ : Set Ω)) n m = fun _ ↦ if Set.Icc n m ∩ I ≠ ∅ then
+      sInf (Set.Icc n m ∩ I) else m := by
+  simp
+
+@[simp]
+lemma univ_prod (A : Set Ω) [DecidablePred (· ∈ A)] :
+    Debut ((.univ : Set ι) ×ˢ A) n m = fun ω ↦ if n ≤ m then
+        if ω ∈ A then n else m
+      else m := by
+  unfold Debut
+  split_ifs with h
+  · ext ω
+    split_ifs with hi hω hω
+    · simpa [hω] using csInf_Icc h
+    · simp_all
+    · simp only [Set.mem_Icc, Set.mem_prod, Set.mem_univ, hω, and_true, not_exists,
+        not_and, not_le] at hi
+      exact hi n (Preorder.le_refl n) |>.not_ge h |>.elim
+    · simp_all
+  · simp [h]
+
+lemma anti : Antitone (Debut (Ω := Ω) · n m) := by
+  intro E F h ω
+  simp only [Debut]
+  split_ifs with hF hE hE
+  · exact csInf_le_csInf ⟨n, fun i hi ↦ hi.1.1⟩ hE (by aesop)
+  · have ⟨t, ht⟩ := hF
+    exact csInf_le_of_le ⟨n, fun i hi ↦ hi.1.1⟩ ht ht.1.2
+  · have ⟨t, ht⟩ := hE
+    exact (hF ⟨t, ⟨ht.1, h ht.2⟩⟩).elim
+  · exact Preorder.le_refl m
+
+section Inequalities
+
+lemma le (E : Set (ι × Ω)) (ω : Ω) : Debut E n m ω ≤ m := by
+  unfold Debut
+  split_ifs with h
+  · have ⟨t, ht, htE⟩ := h
+    exact csInf_le_of_le (BddBelow.inter_of_left bddBelow_Icc) ⟨ht, htE⟩ ht.2
+  · exact le_rfl
+
+lemma ge (E : Set (ι × Ω)) {n m : ι} (ω : Ω) (hnm : n ≤ m) : n ≤ Debut E n m ω := by
+  unfold Debut
+  split_ifs with h
+  · exact le_csInf h fun i hi ↦ hi.1.1
+  · exact hnm
+
+lemma ge_of_exists {E : Set (ι × Ω)} {n m : ι} {ω : Ω} (h : ∃ t ∈ Set.Icc n m, (t, ω) ∈ E) :
+    n ≤ Debut E n m ω := by
+  refine ge E ω ?_
+  by_contra! hh
+  simp [hh] at h
+
+lemma of_le (E : Set (ι × Ω)) {n m : ι} (ω : Ω) (h : m ≤ n) : Debut E n m ω = m := by
+  rcases eq_or_lt_of_le h with rfl | h
+  · exact le_antisymm (le ..) (ge _ _ h)
+  · grind [Debut, not_le, Set.Icc_eq_empty, Set.mem_empty_iff_false]
+
+lemma mem_Icc (E : Set (ι × Ω)) {n m : ι} (ω : Ω) (hnm : n ≤ m) : Debut E n m ω ∈ Set.Icc n m :=
+  ⟨ge E ω hnm, le ..⟩
+
+lemma mem_set [WellFoundedLT ι] {E : Set (ι × Ω)} {n m : ι} {ω : Ω}
+    (h : ∃ t ∈ Set.Icc n m, (t, ω) ∈ E) :
+    (Debut E n m ω, ω) ∈ E := by
+  rw [Debut, if_pos h]
+  exact (csInf_mem h).2
+
+lemma mem_set_of_debut_lt [WellFoundedLT ι] {E : Set (ι × Ω)} {n m : ι} {ω : Ω}
+    (h : Debut E n m ω < m) :
+    (Debut E n m ω, ω) ∈ E := by
+  refine mem_set ?_
+  by_contra ht
+  simp [Debut, if_neg ht] at h
+
+lemma le_of_mem {E : Set (ι × Ω)} {n m i : ι} (hni : n ≤ i) (him : i ≤ m) {ω : Ω}
+    (hiE : (i, ω) ∈ E) :
+    Debut E n m ω ≤ i := by
+  have h_exists : ∃ k ∈ Set.Icc n m, (k, ω) ∈ E := ⟨i, ⟨hni, him⟩, hiE⟩
+  simp_rw [Debut, if_pos h_exists]
+  exact csInf_le (BddBelow.inter_of_left bddBelow_Icc) (Set.mem_inter ⟨hni, him⟩ hiE)
+
+lemma eq_debut_of_exists {E : Set (ι × Ω)} {n m₁ m₂ : ι} {ω : Ω}
+    (hm : m₁ ≤ m₂) (h : ∃ t ∈ Set.Icc n m₁, (t, ω) ∈ E) :
+    Debut E n m₁ ω = Debut E n m₂ ω := by
+  simp only [Debut, if_pos h]
+  have ⟨t, ht, htE⟩ := h
+  rw [if_pos ⟨t, ⟨ht.1, ht.2.trans hm⟩, htE⟩]
+  refine le_antisymm ?_ (csInf_le_csInf bddBelow_Icc.inter_of_left ⟨t, ht, htE⟩
+      (Set.inter_subset_inter_left _ (Set.Icc_subset_Icc_right hm)))
+  refine le_csInf ⟨t, Set.Icc_subset_Icc_right hm ht, htE⟩ fun i hi => ?_
+  by_cases him : i ≤ m₁
+  · exact csInf_le bddBelow_Icc.inter_of_left ⟨⟨hi.1.1, him⟩, hi.2⟩
+  · exact ((csInf_le bddBelow_Icc.inter_of_left ⟨ht, htE⟩).trans (ht.2.trans le_rfl)).trans
+        (le_of_lt (not_le.1 him))
+
+lemma mono_right (E : Set (ι × Ω)) (n : ι) (ω : Ω) : Monotone (Debut E n · ω) := by
+  refine fun m₁ m₂ hm ↦ ?_
+  by_cases h : ∃ j ∈ Set.Icc n m₁, (j, ω) ∈ E
+  · exact (eq_debut_of_exists hm h).le
+  · simp_rw [Debut, if_neg h]
+    split_ifs with h'
+    · have ⟨t, ht, htE⟩ := h'
+      refine le_csInf h' fun t ht ↦ ?_
+      by_contra! htm
+      exact h ⟨t, ⟨ht.1.1, htm.le⟩, ht.2⟩
+    · exact hm
+
+end Inequalities
+
+-- TODO: this may be put in a separate file, maybe in the file where ProgMeasurable is?
+/- TODO: can we find some condition equivalent to this definition that is easier to state, maybe something that does not involde the indicator function and only uses E as a set? Maybe there is a σ algebra over `ι × Ω` such that ProgMeasurableSet is equivalent to being measurable with respect to that σ-algebra?
+maybe something like (mι : MeasurableSpace ι) [BorelSpace ι] `∀ t, MeasurableSet[mι.prod (f t)] (E ∩ Set.Iic t ×ˢ Ω)`? I'm not completely sure this is actually equivalent, but if I stated the lemma `MeasureTheory.Approximation.of_mem_prod_borel` correctly this should be enough to prove the theorem below.
+before changing this definition it may be worth it to begin the proof of `Debut.isStoppingTime` to identify exactly what is needed, maybe in the end we do not even need to define the concpet of progressively measurable set, but we can just add the necessary hypothesis manually. -/
+
+/-- A set `E : Set ι × Ω` is *Progressively measurable* with respect to a filtration `f` if the indicator function of `E` is a progressively measurable process with respect to `f`. -/
+def _root_.MeasureTheory.ProgMeasurableSet [MeasurableSpace ι] (E : Set (ι × Ω)) (f : Filtration ι mΩ) := ProgMeasurable f (E.indicator fun _ ↦ 1).curry
+
+/-- **Debùt Therorem**: The debut of a progressively measurable set `E` is a stopping time. -/
+theorem isStoppingTime [MeasurableSpace ι]
+    {E : Set (ι × Ω)} {f : Filtration ι mΩ} (hE : ProgMeasurableSet E f) :
+    IsStoppingTime f (Debut E n m) := by
+  -- see the proof in the blueprint, we will probably need some more hypotheses, for example the usual hypotheses on the filtration (in particular the right continuity of the filtration, find if it is defined anywhere in mathlib, or if we need to define it ourselves or just state it as a hypothesis)
+  sorry
+
+end Debut
+
+section HittingTime
+
+-- This may be placed in `Mathlib.Probability.Process.HittingTime` in Mathlib.
+-- We may need to add some hypotheses, like the filtration being right continuous. After proving the theorem consider if this completely subsumes `hitting_isStoppingTime`, in that case we can remove the latter. Also, consider if the fact that `β` is a borel space is actually needed.
+theorem hitting_isStoppingTime' [ConditionallyCompleteLinearOrder ι] [MeasurableSpace ι]
+    {β : Type*} [TopologicalSpace β] [MeasurableSpace β] [BorelSpace β]
+    {f : Filtration ι mΩ} {X : ι → Ω → β} {s : Set β} {n m : ι} (hX : ProgMeasurable f X) (hs : MeasurableSet s) :
+    IsStoppingTime f (hitting X s n m) := by
+  sorry
+
+end HittingTime
+
+
+end MeasureTheory
