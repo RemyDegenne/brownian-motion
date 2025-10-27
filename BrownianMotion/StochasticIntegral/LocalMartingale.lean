a9b959eb943fdd24ec7ff5037fa7e14e87e00eef
Remy Degenne <remydegenne@gmail.com>
2025-10-27 16:50:40 +0100

    update imports

--- BrownianMotion/StochasticIntegral/LocalMartingale.lean
+++ BrownianMotion/StochasticIntegral/LocalMartingale.lean
@@ -4,6 +4,7 @@ Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
 import BrownianMotion.StochasticIntegral.Locally
+import Mathlib.Probability.Martingale.Basic
 
 /-! # Local (sub)martingales
 
--- BrownianMotion/StochasticIntegral/Locally.lean
+++ BrownianMotion/StochasticIntegral/Locally.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import BrownianMotion.StochasticIntegral.MathlibImports
+import Mathlib.Probability.Process.Stopping
 
 /-! # Local properties of processes
 
