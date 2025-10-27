21b89b07f367790e422653a40719f626441fe7b1
Remy Degenne <remydegenne@gmail.com>
2025-10-27 16:45:57 +0100

    adapt to new stopping times

--- BrownianMotion/StochasticIntegral/Locally.lean
+++ BrownianMotion/StochasticIntegral/Locally.lean
@@ -10,7 +10,7 @@ import BrownianMotion.StochasticIntegral.MathlibImports
 -/
 
 open MeasureTheory Filter
-open scoped ENNReal
+open scoped ENNReal Topology
 
 namespace ProbabilityTheory
 
@@ -18,28 +18,39 @@ variable {ι Ω E : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω}
 
 /-- A localizing sequence is a sequence of stopping times that is almost surely increasing and
 tends almost surely to infinity. -/
-structure IsLocalizingSequence [Preorder ι] (𝓕 : Filtration ι mΩ) (τ : ℕ → Ω → ι)
+structure IsLocalizingSequence [Preorder ι] (𝓕 : Filtration ι mΩ) (τ : ℕ → Ω → WithTop ι)
     (P : Measure Ω := by volume_tac) :
     Prop where
   isStoppingTime : ∀ n, IsStoppingTime 𝓕 (τ n)
   mono : ∀ᵐ ω ∂P, Monotone (τ · ω)
   tendsto_top : ∀ᵐ ω ∂P, Tendsto (τ · ω) atTop atTop
 
+lemma isLocalizingSequence_const_top [Preorder ι] (𝓕 : Filtration ι mΩ) (P : Measure Ω) :
+    IsLocalizingSequence 𝓕 (fun _ _ ↦ ⊤) P :=
+  { isStoppingTime n := by simp [IsStoppingTime]
+    mono := ae_of_all _ fun _ _ _ _ ↦ by simp
+    tendsto_top := by filter_upwards [] with ω using by simp [tendsto_atTop] }
+
 variable [LinearOrder ι] [OrderBot ι] {𝓕 : Filtration ι mΩ} {X : ι → Ω → E}
   {p q : (ι → Ω → E) → Prop}
+
+@[simp]
+lemma stoppedProcess_const_top : stoppedProcess X (fun _ ↦ ⊤) = X := by
+  unfold stoppedProcess
+  simp
 
 /-- A stochastic process `X` is said to satisfy a property `p` locally with respect to a
 filtration `𝓕` if there exists a localizing sequence `(τ_n)` such that for all `n`, the stopped
 process of `fun i ↦ {ω | ⊥ < τ n ω}.indicator (X i)` satisfies `p`. -/
 def Locally [Zero E] (p : (ι → Ω → E) → Prop) (𝓕 : Filtration ι mΩ)
     (X : ι → Ω → E) (P : Measure Ω := by volume_tac) : Prop :=
-  ∃ τ : ℕ → Ω → ι, IsLocalizingSequence 𝓕 τ P ∧
+  ∃ τ : ℕ → Ω → WithTop ι, IsLocalizingSequence 𝓕 τ P ∧
     ∀ n, p (stoppedProcess (fun i ↦ {ω | ⊥ < τ n ω}.indicator (X i)) (τ n))
 
 /-- A localizing sequence, witness of the local property of the stochastic process. -/
 noncomputable
 def Locally.localizingSequence [Zero E] (hX : Locally p 𝓕 X P) :
-    ℕ → Ω → ι :=
+    ℕ → Ω → WithTop ι :=
   hX.choose
 
 lemma Locally.IsLocalizingSequence [Zero E] (hX : Locally p 𝓕 X P) :
@@ -51,9 +62,8 @@ lemma Locally.stoppedProcess [Zero E] (hX : Locally p 𝓕 X P) (n : ℕ) :
       (hX.localizingSequence n)) :=
   hX.choose_spec.2 n
 
--- needs the stopping time refactor
-lemma locally_of_prop [Zero E] (hp : p X) : Locally p 𝓕 X P := by
-  sorry
+lemma locally_of_prop [Zero E] (hp : p X) : Locally p 𝓕 X P :=
+  ⟨fun n _ ↦ (⊤ : WithTop ι), isLocalizingSequence_const_top _ _, by simpa⟩
 
 lemma Locally.mono [Zero E] (hpq : ∀ X, p X → q X) (hpX : Locally p 𝓕 X P) :
     Locally q 𝓕 X P := by
@@ -62,7 +72,8 @@ lemma Locally.mono [Zero E] (hpq : ∀ X, p X → q X) (hpX : Locally p 𝓕 X P
 /-- A property of stochastic processes is said to be stable if it is preserved under taking
 the stopped process by a stopping time. -/
 def IsStable [Zero E] (p : (ι → Ω → E) → Prop) (𝓕 : Filtration ι mΩ) : Prop :=
-  ∀ X : ι → Ω → E, p X → ∀ τ : Ω → ι, IsStoppingTime 𝓕 τ → p (fun i ↦ {ω | ⊥ < τ ω}.indicator (X i))
+  ∀ X : ι → Ω → E, p X → ∀ τ : Ω → WithTop ι, IsStoppingTime 𝓕 τ →
+    p (fun i ↦ {ω | ⊥ < τ ω}.indicator (X i))
 
 lemma locally_and [Zero E] (hp : IsStable p 𝓕) (hq : IsStable q 𝓕) :
     Locally (fun Y ↦ p Y ∧ q Y) 𝓕 X P ↔ Locally p 𝓕 X P ∧ Locally q 𝓕 X P := by
