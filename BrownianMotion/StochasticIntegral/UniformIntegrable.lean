db594d9bff46279cae2066eb65bbfd57e98fb331
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-06 23:13:25 +0100

    tidy

--- BrownianMotion/StochasticIntegral/UniformIntegrable.lean
+++ BrownianMotion/StochasticIntegral/UniformIntegrable.lean
@@ -77,26 +77,8 @@ lemma UniformIntegrable.condExp (hX : UniformIntegrable X 1 μ)
     UniformIntegrable (fun i ↦ μ[X i | 𝓕 i]) 1 μ :=
   (hX.condExp' h𝓕).comp (fun i ↦ (i, i))
 
-variable [Preorder ι]
-
-variable {ι : Type*} [Nonempty ι]
-
-theorem uniformIntegrable_subsingleton' {p : ℝ≥0∞} {f : ι → Ω → E} [NormedAddCommGroup E]
-    (hp_one : 1 ≤ p) (hp_top : p ≠ ∞)
-    (hf : ∀ i, f i =ᵐ[μ] f (Classical.arbitrary ι))
-    (hfint : MemLp (f (Classical.arbitrary ι)) p μ) : UniformIntegrable f p μ := by
-  refine ⟨fun i ↦ AEStronglyMeasurable.congr hfint.1 (hf i).symm, ?_, ?_⟩
-  · intro ε hε
-    obtain ⟨δ, hδpos, hδ⟩ := unifIntegrable_subsingleton hp_one hp_top
-      (f := fun (i : Fin 1) ↦ f (Classical.arbitrary ι)) (fun i ↦ hfint) hε
-    exact ⟨δ, hδpos, fun i s hs hμs ↦
-      le_trans (le_of_eq <| eLpNorm_congr_ae (hf i).indicator) <| hδ 0 s hs hμs⟩
-  · refine ⟨(eLpNorm (f (Classical.arbitrary ι)) p μ).toNNReal, fun i ↦ ?_⟩
-    rw [eLpNorm_congr_ae (hf i)]
-    exact le_of_eq <| (ENNReal.coe_toNNReal hfint.eLpNorm_ne_top).symm
-
-variable [LinearOrder ι] [OrderBot ι] [Countable ι] [TopologicalSpace ι] [OrderTopology ι]
-  [FirstCountableTopology ι] {𝓕 : Filtration ι mΩ} [SigmaFiniteFiltration μ 𝓕]
+variable {ι : Type*} [LinearOrder ι] [OrderBot ι] [Countable ι] [TopologicalSpace ι]
+  [OrderTopology ι] [FirstCountableTopology ι] {𝓕 : Filtration ι mΩ} [SigmaFiniteFiltration μ 𝓕]
 
 lemma Martingale.ae_eq_condExp_of_isStoppingTime {X : ι → Ω → ℝ}
     (hX : Martingale X 𝓕 μ) {τ : Ω → WithTop ι} (hτ : IsStoppingTime 𝓕 τ) {n : ι}
