ce1a7ec2c08b09e3f4dd317b8019aea7d6cf6726
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-06 22:02:36 +0100

    add lemma and update blueprint

--- BrownianMotion/StochasticIntegral/Centering.lean
+++ BrownianMotion/StochasticIntegral/Centering.lean
@@ -15,17 +15,30 @@ open scoped NNReal ENNReal
 namespace MeasureTheory
 
 variable {Ω E : Type*} {mΩ : MeasurableSpace Ω} {μ : Measure Ω}
-  [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E] [SecondCountableTopology E]
-  [MeasurableSpace E] [BorelSpace E]
+  [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
   {X : ℕ → Ω → E} {𝓕 : Filtration ℕ mΩ}
+
+lemma predictablePart_succ (n : ℕ) :
+    predictablePart X 𝓕 μ (n + 1) =
+      predictablePart X 𝓕 μ n + μ[X (n + 1) - X n | 𝓕 n] := by
+  induction n with
+  | zero => simp [predictablePart]
+  | succ k hk =>
+    simp [predictablePart, Finset.sum_range_add]
+
+variable [SecondCountableTopology E] [MeasurableSpace E] [BorelSpace E]
 
 lemma isPredictable_predictablePart : IsPredictable 𝓕 (predictablePart X 𝓕 μ) :=
   isPredictable_of_measurable_add_one (by simp [measurable_const'])
     fun n ↦ (adapted_predictablePart n).measurable
 
 -- todo: feel free to replace `Preorder E` by something stonger if needed
-lemma Submartingale.monotone_predictablePart [Preorder E] (hX : Submartingale X 𝓕 μ) :
-    Monotone (predictablePart X 𝓕 μ) := by
-  sorry
+lemma Submartingale.monotone_predictablePart {X : ℕ → Ω → ℝ} (hX : Submartingale X 𝓕 μ) :
+    ∀ᵐ ω ∂μ, Monotone (predictablePart X 𝓕 μ · ω) := by
+  have := ae_all_iff.2 <| fun n : ℕ ↦ hX.condExp_sub_nonneg n.le_succ
+  filter_upwards [this] with ω h
+  simp only [Pi.zero_apply, Nat.succ_eq_add_one, ← ge_iff_le] at h
+  refine monotone_nat_of_le_succ fun n ↦ (?_ : _ ≥ _)
+  grw [predictablePart_succ, Pi.add_apply, h n, add_zero]
 
 end MeasureTheory
