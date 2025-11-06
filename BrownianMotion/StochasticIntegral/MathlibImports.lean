8bfa4ecf90ae1608e2293edb6cbde50626d083c0
Remy Degenne <remydegenne@gmail.com>
2025-11-06 10:35:24 +0100

    add Lean statements

--- BrownianMotion/StochasticIntegral/MathlibImports.lean
+++ BrownianMotion/StochasticIntegral/MathlibImports.lean
@@ -1,6 +1,4 @@
-import Mathlib.Probability.Martingale.Centering
 import Mathlib.Probability.Martingale.Convergence
 import Mathlib.Probability.Martingale.OptionalStopping
-import Mathlib.Probability.Martingale.OptionalSampling
 -- the goal of this file is to import statements that are mentioned in the blueprint but are not
 -- used yet in the code
