9bb375443cb5cb5dc361baf8d3b39d2f2463076d
Remy Degenne <remydegenne@gmail.com>
2025-11-11 11:54:47 +0100

    mk_all

--- BrownianMotion.lean
+++ BrownianMotion.lean
@@ -6,6 +6,7 @@ import BrownianMotion.Auxiliary.FiniteInf
 import BrownianMotion.Auxiliary.HasGaussianLaw
 import BrownianMotion.Auxiliary.HasLaw
 import BrownianMotion.Auxiliary.IsStoppingTime
+import BrownianMotion.Auxiliary.Jensen
 import BrownianMotion.Auxiliary.LinearAlgebra
 import BrownianMotion.Auxiliary.Martingale
 import BrownianMotion.Auxiliary.MeanInequalities
