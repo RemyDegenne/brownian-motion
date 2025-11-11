5dcbd29bbaa96577311fa5b3434cb10bce57a77b
Remy Degenne <remydegenne@gmail.com>
2025-11-11 14:55:10 +0100

    change import

--- BrownianMotion/StochasticIntegral/DoobLp.lean
+++ BrownianMotion/StochasticIntegral/DoobLp.lean
@@ -3,7 +3,7 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import BrownianMotion.Auxiliary.Jensen
+import BrownianMotion.Auxiliary.Martingale
 import Mathlib.Probability.Martingale.OptionalStopping
 
 /-! # Doob's Lᵖ inequality
