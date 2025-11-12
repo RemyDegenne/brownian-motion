e8f0e80f70c0f813e1937ed40017474b2492ec71
Remy Degenne <remydegenne@gmail.com>
2025-11-12 20:18:43 +0100

    add verso

--- BrownianMotion.lean
+++ BrownianMotion.lean
@@ -37,3 +37,4 @@ import BrownianMotion.StochasticIntegral.Locally
 import BrownianMotion.StochasticIntegral.MathlibImports
 import BrownianMotion.StochasticIntegral.Predictable
 import BrownianMotion.StochasticIntegral.SimpleProcess
+import BrownianMotion.Verso.Processes
