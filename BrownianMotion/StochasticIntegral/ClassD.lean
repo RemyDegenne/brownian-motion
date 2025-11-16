71455ff92f6f37678fb7743fed28efbc52cc48b9
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-16 12:31:10 +0100

    revert

--- BrownianMotion/StochasticIntegral/ClassD.lean
+++ BrownianMotion/StochasticIntegral/ClassD.lean
@@ -3,15 +3,13 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
-import BrownianMotion.Auxiliary.Martingale
-import BrownianMotion.StochasticIntegral.ApproxSeq
 import BrownianMotion.StochasticIntegral.Locally
 
 /-! # Locally integrable, class D, class DL
 
 -/
 
-open MeasureTheory Filter Function TopologicalSpace
+open MeasureTheory Filter
 open scoped ENNReal
 
 namespace ProbabilityTheory
@@ -25,41 +23,5 @@ for all `t`, the random variable `ω ↦ sup_{s ≤ t} ‖X s ω‖` is integrab
 def HasLocallyIntegrableSup (X : ι → Ω → E) (𝓕 : Filtration ι mΩ)
     (P : Measure Ω := by volume_tac) : Prop :=
   Locally (fun Y ↦ ∀ t, Integrable (fun ω ↦ ⨆ s ≤ t, ‖Y s ω‖) P) 𝓕 X P
-
-section Classes
-
-variable {ι : Type*} [Preorder ι] [Nonempty ι]
-
-structure ClassD (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure Ω) :
-    Prop where
-  adapted : Adapted 𝓕 X
-  uniformIntegrable : UniformIntegrable
-    (fun (τ : {T : Ω → WithTop ι | IsStoppingTime 𝓕 T ∧ ∀ ω, T ω ≠ ⊤}) ↦ stoppedValue X τ.1) 1 P
-
-structure ClassDL (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure Ω) :
-    Prop where
-  adapted : Adapted 𝓕 X
-  uniformIntegrable (t : ι) : UniformIntegrable
-    (fun (τ : {T : Ω → WithTop ι | IsStoppingTime 𝓕 T ∧ ∀ ω, T ω ≤ t}) ↦ stoppedValue X τ.1) 1 P
-
-lemma ClassD.classDL {𝓕 : Filtration ι mΩ} {X : ι → Ω → E} (hX : ClassD 𝓕 X P) :
-    ClassDL 𝓕 X P := by
-  sorry
-
-variable {ι : Type*} [LinearOrder ι] [TopologicalSpace ι] [OrderTopology ι]
-  [OrderBot ι] [MeasurableSpace ι] [SecondCountableTopology ι] [BorelSpace ι] [MetrizableSpace ι]
-  {𝓕 : Filtration ι mΩ} {X : ι → Ω → E}
-
-section Order
-
-variable [PartialOrder E] [OrderClosedTopology E] [IsOrderedAddMonoid E] [IsOrderedModule ℝ E]
-
-lemma Submartingale.classDL (hX1 : Submartingale X 𝓕 P) (hX2 : RightContinuous X)
-    (hX3 : 0 ≤ X) :
-    ClassDL 𝓕 X P := sorry
-
-end Order
-
-end Classes
 
 end ProbabilityTheory
