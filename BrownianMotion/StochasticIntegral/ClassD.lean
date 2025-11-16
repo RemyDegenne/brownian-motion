1efd286ceeb756ad08a738c370788c55d2e3ba82
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-16 14:07:12 +0100

    feat: add definitions of classes D and DL

--- BrownianMotion/StochasticIntegral/ClassD.lean
+++ BrownianMotion/StochasticIntegral/ClassD.lean
@@ -3,13 +3,15 @@ Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
 Authors: Rémy Degenne
 -/
+import BrownianMotion.Auxiliary.Martingale
+import BrownianMotion.StochasticIntegral.ApproxSeq
 import BrownianMotion.StochasticIntegral.Locally
 
 /-! # Locally integrable, class D, class DL
 
 -/
 
-open MeasureTheory Filter
+open MeasureTheory Filter Function TopologicalSpace
 open scoped ENNReal
 
 namespace ProbabilityTheory
@@ -23,5 +25,23 @@ for all `t`, the random variable `ω ↦ sup_{s ≤ t} ‖X s ω‖` is integrab
 def HasLocallyIntegrableSup (X : ι → Ω → E) (𝓕 : Filtration ι mΩ)
     (P : Measure Ω := by volume_tac) : Prop :=
   Locally (fun Y ↦ ∀ t, Integrable (fun ω ↦ ⨆ s ≤ t, ‖Y s ω‖) P) 𝓕 X P
+
+section Classes
+
+variable {ι : Type*} [Preorder ι] [Nonempty ι]
+
+structure ClassD (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure Ω) :
+    Prop where
+  adapted : Adapted 𝓕 X
+  uniformIntegrable : UniformIntegrable
+    (fun (τ : {T : Ω → WithTop ι | IsStoppingTime 𝓕 T ∧ ∀ ω, T ω ≠ ⊤}) ↦ stoppedValue X τ.1) 1 P
+
+structure ClassDL (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure Ω) :
+    Prop where
+  adapted : Adapted 𝓕 X
+  uniformIntegrable (t : ι) : UniformIntegrable
+    (fun (τ : {T : Ω → WithTop ι | IsStoppingTime 𝓕 T ∧ ∀ ω, T ω ≤ t}) ↦ stoppedValue X τ.1) 1 P
+
+end Classes
 
 end ProbabilityTheory
