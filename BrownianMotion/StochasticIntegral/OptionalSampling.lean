3ec3a8be4d424be946462584df455ce529f23bea
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-16 09:32:26 +0100

    wip

--- BrownianMotion/StochasticIntegral/OptionalSampling.lean
+++ BrownianMotion/StochasticIntegral/OptionalSampling.lean
@@ -7,20 +7,35 @@ namespace MeasureTheory
 
 variable {Ω E : Type*} {mΩ : MeasurableSpace Ω} {P : Measure Ω}
     [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
-    {σ τ : Ω → WithTop ℕ} {X : ℕ → Ω → E} (𝓕 : Filtration ℕ mΩ)
 
-theorem Submartingale.stoppedValue_min_ae_le_condExp [PartialOrder E] [OrderClosedTopology E]
+section Nat
+
+variable {σ τ : Ω → WithTop ℕ} {X : ℕ → Ω → E} (𝓕 : Filtration ℕ mΩ)
+
+theorem Submartingale.stoppedValue_min_ae_le_condExp_nat [PartialOrder E] [OrderClosedTopology E]
     [IsOrderedModule ℝ E] [IsOrderedAddMonoid E]
     (hX : Submartingale X 𝓕 P) {k : ℕ} (hτk : ∀ᵐ ω ∂P, τ ω ≤ k)
     (hσ : IsStoppingTime 𝓕 σ) (hτ : IsStoppingTime 𝓕 τ) :
     stoppedValue X (τ ⊓ σ) ≤ᵐ[P] P[stoppedValue X τ|hσ.measurableSpace] := by
   sorry
 
-theorem Supermartingale.condExp_ae_le_stoppedValue_min [PartialOrder E] [OrderClosedTopology E]
+theorem Supermartingale.condExp_ae_le_stoppedValue_min_nat [PartialOrder E] [OrderClosedTopology E]
     [IsOrderedModule ℝ E] [IsOrderedAddMonoid E]
     (hX : Supermartingale X 𝓕 P) {k : ℕ} (hτk : ∀ᵐ ω ∂P, τ ω ≤ k)
     (hσ : IsStoppingTime 𝓕 σ) (hτ : IsStoppingTime 𝓕 τ) :
     P[stoppedValue X τ|hσ.measurableSpace] ≤ᵐ[P] stoppedValue X (τ ⊓ σ) := by
+  sorry
+
+end Nat
+
+variable {ι : Type*} [LinearOrder ι] [Nonempty ι]
+  {σ τ : Ω → WithTop ι} {X : ι → Ω → E} (𝓕 : Filtration ι mΩ)
+
+theorem Submartingale.stoppedValue_min_ae_le_condExp [PartialOrder E] [OrderClosedTopology E]
+    [IsOrderedModule ℝ E] [IsOrderedAddMonoid E]
+    (hX : Submartingale X 𝓕 P) {k : ι} (hτk : ∀ᵐ ω ∂P, τ ω ≤ k)
+    (hσ : IsStoppingTime 𝓕 σ) (hτ : IsStoppingTime 𝓕 τ) :
+    stoppedValue X (τ ⊓ σ) ≤ᵐ[P] P[stoppedValue X τ|hσ.measurableSpace] := by
   sorry
 
 end MeasureTheory
