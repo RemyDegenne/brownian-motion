8c4ee69d6cbc1a259d36405a815407c0a9826c22
Etienne Marion <baguettes_casino0c@icloud.com>
2025-11-16 17:54:07 +0100

    wip

--- BrownianMotion/StochasticIntegral/ClassD.lean
+++ BrownianMotion/StochasticIntegral/ClassD.lean
@@ -16,23 +16,25 @@ open scoped ENNReal
 
 namespace ProbabilityTheory
 
-variable {ι Ω E : Type*} [LinearOrder ι] [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
+variable {ι Ω E : Type*}
   [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
-  {mΩ : MeasurableSpace Ω} {P : Measure Ω} {X : ι → Ω → E} {𝓕 : Filtration ι mΩ}
+  {mΩ : MeasurableSpace Ω} {P : Measure Ω}
+  {X : ι → Ω → E}
 
 /-- A stochastic process has locally integrable supremum if it satisfies locally the property that
 for all `t`, the random variable `ω ↦ sup_{s ≤ t} ‖X s ω‖` is integrable. -/
-def HasLocallyIntegrableSup (X : ι → Ω → E) (𝓕 : Filtration ι mΩ)
+def HasLocallyIntegrableSup [LinearOrder ι] [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
+    (X : ι → Ω → E) (𝓕 : Filtration ι mΩ)
     (P : Measure Ω := by volume_tac) : Prop :=
   Locally (fun Y ↦ ∀ t, Integrable (fun ω ↦ ⨆ s ≤ t, ‖Y s ω‖) P) 𝓕 X P
 
-section Classes
+section Defs
 
-variable {ι : Type*} [Preorder ι] [Nonempty ι]
+variable [Preorder ι] [Nonempty ι]
 
 /-- A stochastic process $(X_t)$ is of class D (or in the Doob-Meyer class) if it is adapted
 and the set $\{X_\tau \mid \tau \text{ is a finite stopping time}\}$ is uniformly integrable. -/
-structure ClassD (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure Ω) :
+structure ClassD (X : ι → Ω → E) (𝓕 : Filtration ι mΩ) (P : Measure Ω) :
     Prop where
   adapted : Adapted 𝓕 X
   uniformIntegrable : UniformIntegrable
@@ -40,19 +42,25 @@ structure ClassD (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure �
 
 /-- A stochastic process $(X_t)$ is of class DL if it is adapted and for all $t$, the set
 $\{X_\tau \mid \tau \text{ is a stopping time with } \tau \le t\}$ is uniformly integrable. -/
-structure ClassDL (𝓕 : Filtration ι mΩ) (X : ι → Ω → E) (P : Measure Ω) :
+structure ClassDL (X : ι → Ω → E) (𝓕 : Filtration ι mΩ) (P : Measure Ω) :
     Prop where
   adapted : Adapted 𝓕 X
   uniformIntegrable (t : ι) : UniformIntegrable
     (fun (τ : {T : Ω → WithTop ι | IsStoppingTime 𝓕 T ∧ ∀ ω, T ω ≤ t}) ↦ stoppedValue X τ.1) 1 P
 
-lemma ClassD.classDL {𝓕 : Filtration ι mΩ} {X : ι → Ω → E} (hX : ClassD 𝓕 X P) :
-    ClassDL 𝓕 X P := by
+lemma ClassD.classDL {𝓕 : Filtration ι mΩ} {X : ι → Ω → E} (hX : ClassD X 𝓕 P) :
+    ClassDL X 𝓕 P := by
   sorry
 
-variable {ι : Type*} [LinearOrder ι] [TopologicalSpace ι] [OrderTopology ι]
-  [OrderBot ι] [MeasurableSpace ι] [SecondCountableTopology ι] [BorelSpace ι] [MetrizableSpace ι]
-  {𝓕 : Filtration ι mΩ} {X : ι → Ω → E}
+end Defs
+
+section PartialOrder
+
+variable [PartialOrder ι] [Nonempty ι] {𝓕 : Filtration ι mΩ}
+
+section RightContinuous
+
+variable [TopologicalSpace ι]
 
 section Order
 
@@ -60,24 +68,52 @@ variable [PartialOrder E] [OrderClosedTopology E] [IsOrderedAddMonoid E] [IsOrde
 
 lemma _root_.MeasureTheory.Submartingale.classDL (hX1 : Submartingale X 𝓕 P)
     (hX2 : RightContinuous X) (hX3 : 0 ≤ X) :
-    ClassDL 𝓕 X P := sorry
+    ClassDL X 𝓕 P := sorry
 
 lemma _root_.MeasureTheory.Submartingale.classD_iff_uniformIntegrable (hX1 : Submartingale X 𝓕 P)
     (hX2 : RightContinuous X) (hX3 : 0 ≤ X) :
-    ClassD 𝓕 X P ↔ UniformIntegrable X 1 P := sorry
+    ClassD X 𝓕 P ↔ UniformIntegrable X 1 P := sorry
 
 end Order
 
 lemma _root_.MeasureTheory.Martingale.classDL (hX1 : Martingale X 𝓕 P) (hX2 : RightContinuous X) :
-    ClassDL 𝓕 X P := sorry
+    ClassDL X 𝓕 P := sorry
 
 lemma _root_.MeasureTheory.Martingale.classD_iff_uniformIntegrable (hX1 : Martingale X 𝓕 P)
     (hX2 : RightContinuous X) :
-    ClassD 𝓕 X P ↔ UniformIntegrable X 1 P := sorry
+    ClassD X 𝓕 P ↔ UniformIntegrable X 1 P := sorry
 
-lemma isStable_classD : IsStable 𝓕 (ClassD (E := E) 𝓕 · P) := by
+end RightContinuous
+
+end PartialOrder
+
+section LinearOrder
+
+variable [LinearOrder ι] {𝓕 : Filtration ι mΩ}
+
+lemma isStable_classD [OrderBot ι] : IsStable 𝓕 (ClassD (E := E) · 𝓕 P) := by
   sorry
 
-end Classes
+lemma Integrable.classDL [Nonempty ι] (hX : ∀ t, Integrable (fun ω ↦ ⨆ s ≤ t, ‖X t ω‖) P) :
+    (ClassDL X 𝓕 P) := by
+  sorry
+
+lemma HasLocallyIntegrableSup.locally_classDL [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
+    (hX1 : HasLocallyIntegrableSup X 𝓕 P) (hX2 : Adapted 𝓕 X) :
+    Locally (ClassDL · 𝓕 P) 𝓕 X P := by
+  sorry
+
+lemma ClassDL.locally_classD [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
+    (hX : ClassDL X 𝓕 P) :
+    Locally (ClassD · 𝓕 P) 𝓕 X P := by
+  sorry
+
+lemma ClassD.HasLocallyIntegrableSup [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
+    (hX1 : RightContinuous X) (hX2 : ClassD X 𝓕 P) :
+    HasLocallyIntegrableSup X 𝓕 P := by
+  sorry
+
+
+end LinearOrder
 
 end ProbabilityTheory
