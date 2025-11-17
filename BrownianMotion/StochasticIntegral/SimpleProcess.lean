2ff56fb7be12745406526aafc04cc271678b1d03
Thomas Zhu <thomas.zhu.sh@gmail.com>
2025-11-17 02:57:41 -0500

    add author header; blueprint decls

--- BrownianMotion/StochasticIntegral/SimpleProcess.lean
+++ BrownianMotion/StochasticIntegral/SimpleProcess.lean
@@ -1,7 +1,7 @@
 /-
 Copyright (c) 2025 Rémy Degenne. All rights reserved.
 Released under Apache 2.0 license as described in the file LICENSE.
-Authors: Rémy Degenne
+Authors: Rémy Degenne, Thomas Zhu
 -/
 import Mathlib.Probability.Process.Predictable
 import Mathlib.Probability.Process.Stopping
