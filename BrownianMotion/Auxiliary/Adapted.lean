1f9d6d660e86c432b3b393c63b5d1bb28c87a6dd
Kexing Ying <kexing.ying@epfl.ch>
2025-11-18 12:30:16 +0100

    fix

--- BrownianMotion/Auxiliary/Adapted.lean
+++ BrownianMotion/Auxiliary/Adapted.lean
@@ -7,16 +7,12 @@ namespace MeasureTheory
 
 variable {ι Ω β : Type*} [TopologicalSpace ι] [TopologicalSpace β]
 
-/-- A stochastic process is right continuous if each of its realizations is right continuous. -/
-abbrev _root_.Function.RightContinuous [PartialOrder ι] (X : ι → Ω → β) :=
-  ∀ ω a, ContinuousWithinAt (X · ω) (Set.Ioi a) a
-
 variable [LinearOrder ι] [MetrizableSpace ι] [SecondCountableTopology ι] [MeasurableSpace ι]
   [OpensMeasurableSpace ι] [PseudoMetrizableSpace β]
   {mΩ : MeasurableSpace Ω} {μ : Measure Ω} {X : ι → Ω → β} {τ : Ω → WithTop ι} {n : ι}
 
 lemma Adapted.progMeasurable_of_rightContinuous {𝓕 : Filtration ι mΩ}
-    (h : Adapted 𝓕 X) (hu_cont : Function.RightContinuous X) :
+    (h : Adapted 𝓕 X) (hu_cont : ∀ ω a, ContinuousWithinAt (X · ω) (Set.Ioi a) a) :
     ProgMeasurable 𝓕 X :=
   sorry
 
