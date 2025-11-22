689eeebd6b0cedb5aa620cbb88ce156e5a1794b5
Rmal <97214596+CoolRmal@users.noreply.github.com>
2025-11-21 23:34:00 -0500

    Update ClassD.lean

--- BrownianMotion/StochasticIntegral/ClassD.lean
+++ BrownianMotion/StochasticIntegral/ClassD.lean
@@ -100,10 +100,66 @@ lemma _root_.MeasureTheory.Submartingale.classDL [SigmaFinite P] (hX1 : Submarti
     rw [← abs_of_nonneg p1, ← p2] at hω
     exact hω
 
-lemma _root_.MeasureTheory.Submartingale.classD_iff_uniformIntegrable
-    [IsFiniteMeasure P] (hX1 : Submartingale X 𝓕 P)
-    (hX2 : ∀ ω, RightContinuous (X · ω)) (hX3 : 0 ≤ X) :
-    ClassD X 𝓕 P ↔ UniformIntegrable X 1 P := sorry
+lemma _root_.MeasureTheory.Submartingale.classD_iff_uniformIntegrable [SigmaFinite P]
+    (hX1 : Submartingale X 𝓕 P) (hX2 : ∀ ω, RightContinuous (X · ω)) (hX3 : 0 ≤ X) :
+    ClassD X 𝓕 P ↔ UniformIntegrable X 1 P := by
+  let S := {T | IsStoppingTime 𝓕 T ∧ ∀ ω, T ω ≠ ⊤}
+  let G (T : S) : (Ω → E) := stoppedValue X T.1
+  refine ⟨fun hp => ?_, fun hq => ?_⟩
+  · let constT (t : ι) : S := ⟨fun ω : Ω => t, ⟨isStoppingTime_const 𝓕 t, by simp⟩⟩
+    have eq : X = G ∘ constT := by ext; simp [constT, G, stoppedValue]
+    simpa [eq] using hp.2.comp constT
+  · refine ⟨hX1.1, ?_⟩
+    -- set of bounded stopping times
+    let B := {T | IsStoppingTime 𝓕 T ∧ ∃ t : ι, ∀ ω, T ω ≤ t}
+    -- set of condtional expectations of `stoppedValue X T.1` for `T ∈ B` is uniformly integrable
+    have hcond := hq.condExp' (fun T : B => IsStoppingTime.measurableSpace_le T.2.1)
+    -- set of `stoppedValue X T.1` for `T ∈ B` is uniformly integrable
+    have hB : UniformIntegrable (fun T : B ↦ stoppedValue X T.1) 1 P := by
+      refine uniformIntegrable_of_dominated le_rfl hcond (fun ⟨T, hT, ⟨t, ht⟩⟩ => ?_)
+        (fun ⟨T, hT, ⟨t, ht⟩⟩ => ⟨⟨t, ⟨T, hT, ⟨t, ht⟩⟩⟩, ?_⟩)
+      · refine ((stronglyMeasurable_stoppedValue_of_le (β := E) ?_ hT ht).mono
+          (𝓕.le' t)).aestronglyMeasurable
+        exact Adapted.progMeasurable_of_rightContinuous hX1.1 hX2
+      · have : stoppedValue X T ≤ᵐ[P] P[stoppedValue X (fun ω => t)|hT.measurableSpace] := by
+          suffices lem : stoppedValue X ((fun ω => t) ⊓ T) ≤ᵐ[P]
+            P[stoppedValue X (fun ω => t)|hT.measurableSpace] from by
+            have : T ⊓ (fun ω => t) = T := by simpa [inf_eq_left] using ht
+            simpa [inf_comm, this] using lem
+          exact hX1.stoppedValue_min_ae_le_condExp 𝓕 hX2
+            (Eventually.of_forall (fun ω => le_rfl)) hT (isStoppingTime_const 𝓕 t)
+        simp only [stoppedValue_const] at this
+        filter_upwards [this] with ω hω
+        apply norm_le_norm_of_abs_le_abs
+        have p1 : 0 ≤ stoppedValue X T ω := by simpa [stoppedValue] using (hX3 (T ω).untopA ω)
+        have p2 : |P[X t|hT.measurableSpace] ω| = P[X t|hT.measurableSpace] ω :=
+          abs_of_nonneg (le_trans p1 hω)
+        rw [← abs_of_nonneg p1, ← p2] at hω
+        exact hω
+    -- set of limit in probability is uniformly integrable
+    have hlim := hB.uniformIntegrable_of_tendsto_ae 1
+    -- stoppedValue of a finite stopping time is a.e limit of sequences in B
+    have (T : S) : ∃ N : ℕ → B,
+      ∀ᵐ ω ∂P, Tendsto (fun n ↦ stoppedValue X (N n).1 ω) atTop (nhds (stoppedValue X T.1 ω)) := by
+      -- We need second countable order topology on `ι` in the following lemma
+      obtain ⟨v, hv⟩ := exists_seq_monotone_tendsto_atTop_atTop ι
+      refine ⟨fun n => ⟨(fun ω => v n) ⊓ T.1, ⟨IsStoppingTime.min ?_ T.2.1, ?_⟩⟩, ?_⟩
+      · exact (isStoppingTime_const 𝓕 (v n))
+      · exact ⟨v n, fun ω => by simp⟩
+      · filter_upwards with ω
+        simp_all only [tendsto_atTop_atTop, tendsto_atTop_nhds]
+        intros U hU _
+        obtain ⟨N, hN⟩ := hv.2 (T.1 ω).untopA
+        refine ⟨N, fun n hn => ?_⟩
+        have : (T.1 ω).untopA = T.1 ω := by simp [WithTop.untopA_eq_untop (T.2.2 ω)]
+        rw [stoppedValue, Pi.inf_apply, ← this, ← WithTop.coe_min (v n) (T.1 ω).untopA,
+          min_eq_right (hN n hn)]
+        simpa using hU
+    let I (T : S) : {g : Ω → E | ∃ N : ℕ → B,
+      ∀ᵐ ω ∂P, Tendsto (fun n ↦ stoppedValue X (N n).1 ω) atTop (nhds (g ω))} :=
+      ⟨stoppedValue X T.1, this T⟩
+    have eq : G = Subtype.val ∘ I := by ext; simp [I, G]
+    exact hlim.comp I
 
 end Order
 
