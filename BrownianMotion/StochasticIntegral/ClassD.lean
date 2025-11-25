75f3fd3ead787f0638d98b67cc1fff1b7bb2a132
nick-kuhn <nikolaskuhn@gmx.de>
2025-11-25 10:38:45 +0100

    Address lint failures

--- BrownianMotion/StochasticIntegral/ClassD.lean
+++ BrownianMotion/StochasticIntegral/ClassD.lean
@@ -20,13 +20,11 @@ namespace ProbabilityTheory
 variable {ι Ω E : Type*} [NormedAddCommGroup E] {mΩ : MeasurableSpace Ω} {P : Measure Ω}
   {X : ι → Ω → E}
 
-  -- Define the Running Supremum Process S
 
-noncomputable def supProcess [LinearOrder ι] [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
-    (X : ι → Ω → E) (p : ι × Ω) : ℝ≥0∞ :=
-  ⨆ s ≤ p.1, ‖X s p.2‖ₑ
-
-def HasIntegrableSup [LinearOrder ι] [OrderBot ι] [TopologicalSpace ι] [OrderTopology ι]
+/-- A stochastic process has integrable supremum if the function `(t, ω) ↦ sup_{s ≤ t} ‖X s ω‖`
+is strongly measurable and if for all `t`, the random variable `ω ↦ sup_{s ≤ t} ‖X s ω‖`
+is integrable. -/
+def HasIntegrableSup [LinearOrder ι] [TopologicalSpace ι] [OrderTopology ι]
     [MeasurableSpace ι] (X : ι → Ω → E)
     (P : Measure Ω := by volume_tac) : Prop :=
   (StronglyMeasurable (fun (tω : ι × Ω) ↦ ⨆ s ≤ tω.1, ‖X s tω.2‖ₑ)) ∧
