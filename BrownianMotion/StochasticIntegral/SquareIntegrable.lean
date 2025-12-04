b7f255b7bb1ff6684962e5193827e2b54ab5a750
Remy Degenne <remydegenne@gmail.com>
2025-12-04 11:22:33 +0100

    add IsSquareIntegrable def

---
+++ BrownianMotion/StochasticIntegral/SquareIntegrable.lean
@@ -0,0 +1,45 @@
+/-
+Copyright (c) 2025 Rémy Degenne. All rights reserved.
+Released under Apache 2.0 license as described in the file LICENSE.
+Authors: Rémy Degenne
+-/
+import BrownianMotion.Auxiliary.Martingale
+import BrownianMotion.StochasticIntegral.ApproxSeq
+import BrownianMotion.StochasticIntegral.Locally
+import BrownianMotion.Auxiliary.Adapted
+import BrownianMotion.StochasticIntegral.OptionalSampling
+import Mathlib.Probability.Process.HittingTime
+
+/-! # Square integrable martingales
+
+-/
+
+open MeasureTheory Filter Function TopologicalSpace
+open scoped ENNReal
+
+namespace ProbabilityTheory
+
+variable {ι Ω E : Type*} [LinearOrder ι] [TopologicalSpace ι]
+  [NormedAddCommGroup E] [NormedSpace ℝ E] [CompleteSpace E]
+  {mΩ : MeasurableSpace Ω} {P : Measure Ω}
+  {X : ι → Ω → E} {𝓕 : Filtration ι mΩ}
+
+/-- A square integrable martingale is a martingale with cadlag paths and uniformly bounded
+second moments. -/
+structure IsSquareIntegrable (X : ι → Ω → E) (𝓕 : Filtration ι mΩ) (P : Measure Ω) : Prop where
+  martingale : Martingale X 𝓕 P
+  cadlag : ∀ ω, IsCadlag (X · ω)
+  bounded : ⨆ i, eLpNorm (X i) 2 P < ∞
+
+lemma IsSquareIntegrable.integrable_sq (hX : IsSquareIntegrable X 𝓕 P) (i : ι) :
+    Integrable (fun ω ↦ ‖X i ω‖ ^ 2) P := by
+  constructor
+  · have hX_meas := StronglyMeasurable.mono (hX.martingale.adapted i) (𝓕.le i)
+    fun_prop
+  · have hX_bound : eLpNorm (X i) 2 P < ∞ := by
+      calc eLpNorm (X i) 2 P
+      _ ≤ ⨆ j, eLpNorm (X j) 2 P := le_iSup (fun j ↦ eLpNorm (X j) 2 P) i
+      _ < ∞ := hX.bounded
+    simpa [HasFiniteIntegral, eLpNorm_lt_top_iff_lintegral_rpow_enorm_lt_top] using hX_bound
+
+end ProbabilityTheory
