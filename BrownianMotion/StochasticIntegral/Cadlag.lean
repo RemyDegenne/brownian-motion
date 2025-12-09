03962819beccd6ca18c90b6b9b4a99d5a19331b5
nick-kuhn <nikolaskuhn@gmx.de>
2025-12-09 20:22:25 +0100

    Removed First Countable hypothesis

--- BrownianMotion/StochasticIntegral/Cadlag.lean
+++ BrownianMotion/StochasticIntegral/Cadlag.lean
@@ -6,9 +6,11 @@ Authors: Rémy Degenne
 import Mathlib.Analysis.SpecificLimits.Basic
 import Mathlib.Data.Nat.Nth
 import Mathlib.Topology.Bases
+import Mathlib.Topology.MetricSpace.Basic
 import Mathlib.Topology.MetricSpace.Pseudo.Defs
 import Mathlib.Topology.MetricSpace.Bounded
 import Mathlib.Topology.Sequences
+import Mathlib.Topology.Order.Basic
 
 /-! # cadlag functions
 
@@ -30,77 +32,66 @@ structure IsCadlag [PartialOrder ι] (f : ι → E) : Prop where
 
 /-- A càdlàg function maps compact sets to bounded sets. -/
 lemma isBounded_image_of_isCadlag_of_isCompact {E : Type*} [LinearOrder ι] [PseudoMetricSpace E]
-    [FirstCountableTopology ι] {f : ι → E} (hf : IsCadlag f) {s : Set ι} (hs : IsCompact s) :
+    {f : ι → E} (hf : IsCadlag f) {s : Set ι} (hs : IsCompact s) :
     IsBounded (f '' s) := by
   by_contra h_nb
   let x := (nonempty_of_not_isBounded h_nb).some
-  have h_distx : ∀ n : ℕ, ∃ y ∈ f '' s, n ≤ dist y x := fun n ↦ by
+  have h_distx : ∀ n : ℕ, ∃ t_n ∈ s, n ≤ dist (f t_n) x := fun n ↦ by
     rw [Metric.isBounded_iff_subset_ball x] at h_nb
     dsimp [Metric.ball] at h_nb
     push_neg at h_nb
     replace h_nb := h_nb n
     rw [Set.not_subset] at h_nb
-    rcases h_nb with ⟨y, hy_in, hy_dist⟩
-    use y, hy_in
-    simpa [Set.mem_setOf_eq, not_lt] using hy_dist
-  choose u hu_in hu_dist using h_distx
-  have h_dist_infty : Tendsto (fun n ↦ dist (u n) x) atTop atTop :=
-    tendsto_atTop_mono hu_dist tendsto_natCast_atTop_atTop
-  choose t ht_in ht_eq using hu_in
-  obtain ⟨t₀, -, φ, hφ_mono, h_lim⟩ := hs.tendsto_subseq ht_in
-  let is_ge := {n | t₀ ≤ t (φ n)}
-  by_cases h_freq_ge : Set.Infinite is_ge
-  · let ψ := Nat.nth is_ge
-    have hψ_mono : StrictMono ψ := Nat.nth_strictMono h_freq_ge
-    have hψ_mem : ∀ n, ψ n ∈ is_ge := Nat.nth_mem_of_infinite h_freq_ge
-    let sub_idx := φ ∘ ψ
-    have h_tendsto_right : Tendsto (t ∘ sub_idx) atTop (𝓝[≥] t₀) := by
-      apply tendsto_nhdsWithin_of_tendsto_nhds_of_eventually_within
-      · exact h_lim.comp hψ_mono.tendsto_atTop
-      · filter_upwards with n using hψ_mem n
-    have h_right_cont := (hf.right_continuous t₀)
-    have h_weak_right : Tendsto f (𝓝[≥] t₀) (𝓝 (f t₀)) := by
-      have h_set_eq : Set.Ici t₀ = insert t₀ (Set.Ioi t₀) := by
-        exact Eq.symm Set.Ioi_insert
-      rw [h_set_eq, nhdsWithin_insert]
-      apply tendsto_sup.mpr
-      constructor
-      · exact tendsto_pure_nhds _ _
-      · exact h_right_cont.tendsto
-    have h_img_conv : Tendsto (f ∘ t ∘ sub_idx) atTop (𝓝 (f t₀)) :=
-      h_weak_right.comp h_tendsto_right
-    have h_dist_conv := h_img_conv.dist (tendsto_const_nhds (x := x))
-    have h_u_sub : (fun n ↦ dist (u (sub_idx n)) x) = (fun n ↦ dist ((f ∘ t ∘ sub_idx) n) x) := by
-      ext n
-      simp only [Function.comp_apply, ht_eq]
-    rw [←h_u_sub] at h_dist_conv
-    have h_dist_boom : Tendsto (fun n ↦ dist (u (sub_idx n)) x) atTop atTop :=
-      h_dist_infty.comp (hφ_mono.comp hψ_mono).tendsto_atTop
-    apply not_tendsto_nhds_of_tendsto_atTop h_dist_boom
-    exact h_dist_conv
-  · rw [Set.not_infinite] at h_freq_ge
-    let is_lt := {n | t (φ n) < t₀}
-    have h_freq_lt : Set.Infinite is_lt := by
-      apply Set.infinite_of_finite_compl
-      convert h_freq_ge using 1
-      ext n
-      simp only [is_lt, Set.mem_compl_iff, Set.mem_setOf_eq, is_ge, not_lt]
-    let ψ := Nat.nth is_lt
-    have hψ_mono : StrictMono ψ := Nat.nth_strictMono h_freq_lt
-    have hψ_mem : ∀ n, ψ n ∈ is_lt := Nat.nth_mem_of_infinite h_freq_lt
-    let sub_idx := φ ∘ ψ
-    have h_tendsto_left : Tendsto (t ∘ sub_idx) atTop (𝓝[<] t₀) := by
-       apply tendsto_nhdsWithin_of_tendsto_nhds_of_eventually_within
-       · exact h_lim.comp hψ_mono.tendsto_atTop
-       · filter_upwards with n using hψ_mem n
-    obtain ⟨y, hy_lim⟩ := (hf.left_limit t₀)
-    have h_img_conv : Tendsto (f ∘ t ∘ sub_idx) atTop (𝓝 y) :=
-      hy_lim.comp h_tendsto_left
-    have h_dist_conv : Tendsto (fun n ↦ dist ((f ∘ t ∘ sub_idx) n) x) atTop (𝓝 (dist y x)) :=
-      h_img_conv.dist (tendsto_const_nhds (x := x))
-    have h_u_sub : (fun n ↦ dist (u (sub_idx n)) x) = (fun n ↦ dist ((f ∘ t ∘ sub_idx) n) x) := by
-      ext n; simp [ht_eq]
-    rw [←h_u_sub] at h_dist_conv
-    have h_seq_boom : Tendsto (fun n ↦ dist (u (sub_idx n)) x) atTop atTop :=
-      h_dist_infty.comp (hφ_mono.comp hψ_mono).tendsto_atTop
-    exact (not_tendsto_nhds_of_tendsto_atTop h_seq_boom _) h_dist_conv
+    rcases h_nb with ⟨y, ⟨t, ht_in, ht_y⟩, hy_dist⟩
+    use t, ht_in
+    simpa [Set.mem_setOf_eq, not_lt, ht_y] using hy_dist
+  choose t ht_in ht_eq using h_distx
+  let V := Ultrafilter.of (map t atTop)
+  have h_map_le_V : V ≤ map t atTop  := Ultrafilter.of_le (map t atTop)
+  have hs_in_V : s ∈ V := by
+    apply h_map_le_V
+    rw [mem_map]
+    filter_upwards [Filter.univ_mem] with n _ using ht_in n
+  rcases IsCompact.ultrafilter_le_nhds' hs V hs_in_V with ⟨c, _, hV_le_nhds⟩
+  have h_loc_bdd : ∃ W ∈ 𝓝 c, Bornology.IsBounded (f '' W) := by
+    obtain ⟨l, hl⟩ := hf.left_limit c
+    rcases Metric.exists_isBounded_image_of_tendsto hl with ⟨U_left, hU_mem, hU_bdd⟩
+    rcases Metric.exists_isBounded_image_of_tendsto (hf.right_continuous c).tendsto
+      with ⟨_, hV_mem, hV_bdd⟩
+    rw [mem_nhdsWithin_iff_exists_mem_nhds_inter] at hU_mem hV_mem
+    rcases hU_mem with ⟨G_left, hGl_nhds, hGl_sub⟩
+    rcases hV_mem with ⟨G_right, hGr_nhds, hGr_sub⟩
+    let W := G_left ∩ G_right
+    have hW_nhds : W ∈ 𝓝 c := Filter.inter_mem hGl_nhds hGr_nhds
+    use W, hW_nhds
+    apply Bornology.IsBounded.subset ((hU_bdd.union hV_bdd).union
+      (isBounded_induced.mp isBounded_singleton : Bornology.IsBounded (f '' {c})))
+    rintro _ ⟨y, ⟨hyL, hyR⟩ , rfl⟩
+    rcases lt_trichotomy y c with (hlt | heq | hgt)
+    · apply Or.inl ∘ Or.inl
+      simp_all only [inter_mem_iff, and_self, Set.mem_image, x, V, W]
+      refine ⟨y, hGl_sub ?_,  rfl⟩
+      simp_all only [Set.mem_inter_iff, Set.mem_Iio, and_self]
+    · apply Or.inr
+      rw [heq]
+      exact Set.mem_image_of_mem f rfl
+    · apply  Or.inl ∘ Or.inr
+      apply (Set.mem_image _ _ _).mpr ⟨y, hGr_sub ?_,  rfl⟩
+      simp_all only [Set.mem_inter_iff, Set.mem_Ioi, and_self]
+  rcases h_loc_bdd with ⟨W, hW_nhds, hW_bdd⟩
+  rcases hW_bdd.subset_ball (f c) with ⟨R, h_subset⟩
+  have h_far : ∀ᶠ n in atTop, dist (f (t n)) x > R + dist (f c) x := by
+    have h_dist_infty : Tendsto (fun n ↦ dist (f (t n)) x) atTop atTop :=
+      tendsto_atTop_mono (ht_eq ·) tendsto_natCast_atTop_atTop
+    rw [tendsto_atTop_atTop] at h_dist_infty
+    rcases h_dist_infty (R + dist (f c) x + 1) with ⟨N, hN⟩
+    apply eventually_atTop.2 ⟨N, fun n hn ↦ ?_ ⟩
+    specialize hN n hn
+    linarith
+  have h_inter : (t '' {n | dist (f (t n)) x > R + dist (f c) x} ∩ W).Nonempty :=
+    Ultrafilter.nonempty_of_mem (inter_mem (h_map_le_V (image_mem_map h_far)) (hV_le_nhds hW_nhds))
+  rcases h_inter with ⟨z, ⟨n, hn_far, rfl⟩, hz_in_W⟩
+  have h_close : dist (f (t n)) (f c) < R := h_subset (Set.mem_image_of_mem f hz_in_W)
+  have h_tri : dist (f (t n)) x ≤ dist (f (t n)) (f c) + dist (f c) x := dist_triangle _ _ _
+  dsimp at hn_far
+  linarith
