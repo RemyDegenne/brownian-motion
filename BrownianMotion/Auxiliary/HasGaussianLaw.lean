e239d3169588ee8a1cd4355a7efeea09eb1afb80
Etienne Marion <baguettes_casino0c@icloud.com>
2026-01-28 17:56:22 +0100

    remove sorries in HasGaussianLaw

--- BrownianMotion/Auxiliary/HasGaussianLaw.lean
+++ BrownianMotion/Auxiliary/HasGaussianLaw.lean
@@ -17,20 +17,20 @@ lemma HasGaussianLaw.charFun_toLp_pi {X : ι → Ω → ℝ} (hX : HasGaussianLa
     (ξ : EuclideanSpace ℝ ι) :
     charFun (P.map (fun ω ↦ toLp 2 (X · ω))) ξ =
       exp (∑ i, ξ i * P[X i] * I - ∑ i, ∑ j, (ξ i : ℂ) * ξ j * (cov[X i, X j; P] / 2)) := by
-  sorry
-  -- have := hX.isProbabilityMeasure
-  -- nth_rw 1 [IsGaussian.charFun_eq', covarianceBilin_apply_pi, EuclideanSpace.real_inner_eq]
-  -- · simp_rw [ofReal_sum, Finset.sum_mul, ← mul_div_assoc, Finset.sum_div,
-  --     integral_complex_ofReal, ← ofReal_mul]
-  --   congrm exp (∑ i, Complex.ofReal (_ * ?_) * I - _)
-  --   rw [integral_map, eval_integral_piLp]
-  --   · simp
-  --   · simp only [id_eq, PiLp.toLp_apply]
-  --     exact fun i ↦ HasGaussianLaw.integrable
-  --   · have := hX.aemeasurable
-  --     fun_prop
-  --   · exact aestronglyMeasurable_id
-  -- · exact fun i ↦ HasGaussianLaw.memLp_two
+  -- sorry
+  have := hX.isProbabilityMeasure
+  nth_rw 1 [(hX.toLp_pi 2).isGaussian_map.charFun_eq', covarianceBilin_apply_pi,
+    EuclideanSpace.real_inner_eq]
+  · simp_rw [ofReal_sum, Finset.sum_mul, ← mul_div_assoc, Finset.sum_div,
+      integral_complex_ofReal, ← ofReal_mul]
+    congrm exp (∑ i, Complex.ofReal (_ * ?_) * I - _)
+    rw [integral_map, eval_integral_piLp]
+    · simp
+    · simp only [id_eq, PiLp.toLp_apply]
+      exact fun i ↦ (hX.eval i).integrable
+    · exact (measurable_toLp 2 _).comp_aemeasurable hX.aemeasurable
+    · exact aestronglyMeasurable_id
+  · exact fun i ↦ (hX.eval i).memLp_two
 
 lemma HasGaussianLaw.charFun_toLp_prodMk {X Y : Ω → ℝ} (hXY : HasGaussianLaw (fun ω ↦ (X ω, Y ω)) P)
     (ξ : WithLp 2 (ℝ × ℝ)) :
@@ -38,26 +38,25 @@ lemma HasGaussianLaw.charFun_toLp_prodMk {X Y : Ω → ℝ} (hXY : HasGaussianLa
       exp ((ξ.fst * P[X] + ξ.snd * P[Y]) * I -
         (ξ.fst ^ 2 * Var[X; P] +
           2 * ξ.fst * ξ.snd * cov[X, Y; P] + ξ.snd ^ 2 * Var[Y; P]) / 2) := by
-  sorry
-  -- have := hXY.isProbabilityMeasure
-  -- nth_rw 1 [IsGaussian.charFun_eq', covarianceBilin_apply_prod]
-  -- · simp only [id_eq, prod_inner_apply, ofLp_fst, RCLike.inner_apply, conj_trivial, mul_comm,
-  --   ofLp_snd, ofReal_add, ofReal_mul, add_div, integral_complex_ofReal, pow_two]
-  --   rw [mul_comm (ξ.fst : ℂ), mul_comm (ξ.snd : ℂ)]
-  --   congrm exp (I * (?_ * _ + ?_ * _) - ?_)
-  --   · rw [integral_map, fst_integral_withLp]
-  --     · simp
-  --     · exact HasGaussianLaw.integrable
-  --     · fun_prop
-  --     · exact aestronglyMeasurable_id
-  --   · rw [integral_map, snd_integral_withLp]
-  --     · simp
-  --     · exact HasGaussianLaw.integrable
-  --     · fun_prop
-  --     · exact aestronglyMeasurable_id
-  --   · ring
-  -- · exact hXY.fst.memLp_two
-  -- · exact hXY.snd.memLp_two
+  have := hXY.isProbabilityMeasure
+  nth_rw 1 [(hXY.toLp_prodMk 2).isGaussian_map.charFun_eq', covarianceBilin_apply_prod]
+  · simp only [id_eq, prod_inner_apply, ofLp_fst, RCLike.inner_apply, conj_trivial, mul_comm,
+    ofLp_snd, ofReal_add, ofReal_mul, add_div, integral_complex_ofReal, pow_two]
+    rw [mul_comm (ξ.fst : ℂ), mul_comm (ξ.snd : ℂ)]
+    congrm exp (I * (?_ * _ + ?_ * _) - ?_)
+    · rw [integral_map, fst_integral_withLp]
+      · simp
+      · exact (hXY.toLp_prodMk 2).integrable
+      · fun_prop
+      · exact aestronglyMeasurable_id
+    · rw [integral_map, snd_integral_withLp]
+      · simp
+      · exact (hXY.toLp_prodMk 2).integrable
+      · fun_prop
+      · exact aestronglyMeasurable_id
+    · ring
+  · exact hXY.fst.memLp_two
+  · exact hXY.snd.memLp_two
 
 end charFun
 
