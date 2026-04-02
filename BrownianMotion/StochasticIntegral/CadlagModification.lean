be0915e58f20c84cfc74edde63bd0f4ed35480d2
Kexing Ying <kexing.ying@epfl.ch>
2026-04-02 15:34:15 +0200

    Sorry free for exists_modification_isCadlag

--- BrownianMotion/StochasticIntegral/CadlagModification.lean
+++ BrownianMotion/StochasticIntegral/CadlagModification.lean
@@ -26,8 +26,6 @@ variable [Approximable 𝓕 μ]
 local notation:25 V " ● " X => SimpleProcess.integral (ContinuousLinearMap.mul ℝ ℝ) V X
 
 lemma exists_modification_left_right_limit [IsFiniteMeasure μ]
-    -- Perhaps finite measure can be generalized by requiring the elementary predictable sets to be
-    -- uniformly integrable?
     (hX : StronglyAdapted 𝓕 X) (hXint : ∀ t, Integrable (X t) μ)
     (hXbdd : ∀ t : ι, ∃ C, ∀ S : ElementaryPredictableSet 𝓕, μ[(S.indicator (1 : ℝ) ● X) t] ≤ C) :
     ∃ Y : ι → Ω → ℝ, (∀ t, Y t =ᵐ[μ] X t) ∧
@@ -36,14 +34,12 @@ lemma exists_modification_left_right_limit [IsFiniteMeasure μ]
       ∃ s : Set ι, s.Countable ∧ ∀ x ∉ s, ∀ ω, ContinuousWithinAt (Y · ω) (Set.Ioi x) x := by
   sorry
 
-#check ContinuousWithinAt
-#check tendsto_nhds_iff_seq_tendsto
-
 lemma exists_modification_isCadlag [IsFiniteMeasure μ]
+    (hι : ∀ t, ∃ seq : ℕ → ι, Tendsto seq atTop (𝓝[>] t))
     (hX : StronglyAdapted 𝓕 X) (hXint : ∀ t, Integrable (X t) μ)
-    (hXRC : ∀ t, ∀ᵐ ω ∂μ, ContinuousWithinAt (X · ω) (Set.Ioi t) t) -- RC in probability
+    (hXRC : ∀ t, TendstoInMeasure μ X (𝓝[>] t) (X t))
     (hXbdd : ∀ t : ι, ∃ C, ∀ S : ElementaryPredictableSet 𝓕, μ[(S.indicator (1 : ℝ) ● X) t] ≤ C) :
-    ∃ Y : ι → Ω → ℝ, (∀ t, Y t =ᵐ[μ] X t) ∧ ∀ ω, IsCadlag (Y · ω) := by
+    ∃ Y : ι → Ω → ℝ, (∀ t, Y t =ᵐ[μ] X t) ∧ (∀ t, Integrable (Y t) μ) ∧ ∀ ω, IsCadlag (Y · ω) := by
   classical
   obtain ⟨Y, hY, hYLL, hYRL, s, hs, hYCont⟩ := exists_modification_left_right_limit hX hXint hXbdd
   set S := ⋃ t ∈ s, {ω | ¬ ContinuousWithinAt (Y · ω) (Set.Ioi t) t} with hSdef
@@ -51,33 +47,38 @@ lemma exists_modification_isCadlag [IsFiniteMeasure μ]
     simp only [ae_iff, not_not, Set.setOf_mem_eq, hSdef, measure_biUnion_null_iff hs]
     intro t ht
     rw [← ae_iff]
-    filter_upwards [hY t, hXRC t] with ω hω hCX
-    refine continuousWithinAt_of_tendsto_eq <| hω ▸ ?_
-    obtain ⟨l, hl⟩ := hYRL t ω
-    -- I'm a bit confused here: the proof says "since X is right continuous in probability,
-    -- Y (t+) = Y t." But, we only have X (t+) = X t and X t = Y t a.e.
-    -- Chaning the tendsto to sequences doesn't work as the argument would be of the form
-    -- ∀ᵐ ω, ∀ seq,... while we want ∀ seq, ∀ᵐ ω,... and the latter is uncountable.
-    sorry
-
-  refine ⟨fun t ω ↦ if ω ∈ S then 0 else Y t ω,
-    fun t ↦ EventuallyEq.trans ?_ (hY t), fun ω ↦ ⟨?_, ?_⟩⟩
-  · filter_upwards [hS] with ω hω
-    simp [if_neg hω]
+    choose l hl using hYRL t
+    suffices l =ᵐ[μ] X t by
+      have : ∀ᵐ ω ∂μ, Tendsto (fun x ↦ Y x ω) (𝓝[>] t) (𝓝 (X t ω)) := by
+        filter_upwards [this] with ω hω
+        simp [← hω, hl _]
+      filter_upwards [this, hY t] with ω hω₁ hω₂
+      rwa [ContinuousWithinAt, hω₂]
+    obtain ⟨_, hseq⟩ := hι t
+    exact tendstoInMeasure_ae_unique ((tendstoInMeasure_of_tendsto_ae
+      (fun _ ↦ (hXint _).1.congr (hY _).symm)
+      (ae_of_all _ <| fun ω ↦ (hl ω).comp hseq)).congr (fun _ ↦ hY _) (by rfl))
+      ((hXRC t).comp hseq)
+  set Z := fun t ω ↦ if ω ∈ S then 0 else Y t ω
+  have hZ : ∀ t, Z t =ᵐ[μ] X t := by
+    refine fun t ↦ EventuallyEq.trans ?_ (hY t)
+    filter_upwards [hS] with ω hω
+    simp [Z, if_neg hω]
+  refine ⟨Z, hZ, fun t ↦ (hXint t).congr <| (hZ _).symm, fun ω ↦ ⟨?_, ?_⟩⟩
   · by_cases hω : ω ∈ S
-    · simp_rw [if_pos hω]
+    · simp_rw [Z, if_pos hω]
       exact Function.isRightContinuous_const _
-    · simp_rw [if_neg hω]
+    · simp_rw [Z, if_neg hω]
       intro t
       simp only [hSdef, Set.mem_iUnion, Set.mem_setOf_eq, exists_prop, not_exists,
         not_and, not_not] at hω
       by_cases ht : t ∈ s
       · exact hω _ ht
-      · refine hYCont t ht ω
+      · exact hYCont t ht ω
   · intros x
     by_cases hω : ω ∈ S
-    · exact ⟨0, by simp [if_pos hω, tendsto_const_nhds]⟩
-    · simp [if_neg hω, hYLL x ω]
+    · exact ⟨0, by simp [Z, if_pos hω, tendsto_const_nhds]⟩
+    · simp [Z, if_neg hω, hYLL x ω]
 
 
 end ProbabilityTheory
