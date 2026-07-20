846bb7a20e3c7fdddbfc793d83d024cc09240ad7
Etienne Marion <baguettes_casino0c@icloud.com>
2026-07-20 17:20:29 +0200

    fix

--- BrownianMotion/StochasticIntegral/SquareIntegrable.lean
+++ BrownianMotion/StochasticIntegral/SquareIntegrable.lean
@@ -5,7 +5,6 @@ Authors: Rémy Degenne
 -/
 module
 
-public import BrownianMotion.Auxiliary.AEEq
 public import BrownianMotion.Auxiliary.Indistinguishable
 public import BrownianMotion.Auxiliary.Martingale
 public import BrownianMotion.StochasticIntegral.LocalMartingale
